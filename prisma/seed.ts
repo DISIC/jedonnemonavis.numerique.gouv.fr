9d882dea546c2e08de19509f76bc99a34bf799e3
Clément <clement@numericite.eu>
2023-09-04 14:30:28 +0200

    feat: prettier husky & CI

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient, Procedure, ProcedureHeader, User } from '@prisma/client';
+import { users } from './seeds/users';
+
+const prisma = new PrismaClient();
+
+async function main() {
+	const promises: Promise<User>[] = [];
+
+	users.forEach(user => {
+		promises.push(
+			prisma.user.upsert({
+				where: {
+					email: user.email
+				},
+				update: {},
+				create: {
+					...user
+				}
+			})
+		);
+	});
+
+	Promise.all(promises).then(responses => {
+		let log: { [key: string]: ProcedureHeader | Procedure | User } = {};
+		responses.forEach(r => {
+			if ('email' in r) log[`user ${r.email}`] = r;
+		});
+		console.log(log);
+	});
+}
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async e => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
