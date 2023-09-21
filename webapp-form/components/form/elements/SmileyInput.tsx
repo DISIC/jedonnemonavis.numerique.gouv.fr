cc0256a1554b135548c3b28cfc3d62a89d54a0eb
Clément <clement@numericite.eu>
2023-09-21 19:08:52 +0200

    fix: refactor for accessibility requirements

--- webapp-form/components/form/elements/SmileyInput.tsx
+++ webapp-form/components/form/elements/SmileyInput.tsx
@@ -10,8 +10,32 @@ type Props = {
 	onChange: (smileySelected: string) => void;
 };
 
+const smileys = [
+	{
+		label: 'Pas bien',
+		value: 'bad',
+		alt: 'Pas bien',
+		img: '/assets/smileys/bad.svg',
+		imgSelected: '/assets/smileys/bad-selected.svg'
+	},
+	{
+		label: 'Moyen',
+		value: 'medium',
+		alt: 'Moyen',
+		img: '/assets/smileys/medium.svg',
+		imgSelected: '/assets/smileys/medium-selected.svg'
+	},
+	{
+		label: 'Très bien',
+		value: 'good',
+		alt: 'Très bien',
+		img: '/assets/smileys/good.svg',
+		imgSelected: '/assets/smileys/good-selected.svg'
+	}
+];
+
 export const SmileyInput = (props: Props) => {
-	const { classes, cx } = useStyles();
+	const { classes, cx } = useStyles({ nbItems: smileys.length });
 	const { label, name, hint, onChange } = props;
 
 	const [smileySelected, setSmileySelected] = useState<string | undefined>(
@@ -21,30 +45,6 @@ export const SmileyInput = (props: Props) => {
 	useEffect(() => {
 		if (!!smileySelected) onChange(smileySelected);
 	}, [smileySelected]);
-
-	const smileys = [
-		{
-			label: 'Pas bien',
-			value: 'bad',
-			alt: 'Pas bien',
-			img: '/assets/smileys/bad.svg',
-			imgSelected: '/assets/smileys/bad-selected.svg'
-		},
-		{
-			label: 'Moyen',
-			value: 'medium',
-			alt: 'Moyen',
-			img: '/assets/smileys/medium.svg',
-			imgSelected: '/assets/smileys/medium-selected.svg'
-		},
-		{
-			label: 'Très bien',
-			value: 'good',
-			alt: 'Très bien',
-			img: '/assets/smileys/good.svg',
-			imgSelected: '/assets/smileys/good-selected.svg'
-		}
-	];
 
 	return (
 		<div>
@@ -59,36 +59,37 @@ export const SmileyInput = (props: Props) => {
 					<legend className={fr.cx('fr-sr-only')}>
 						Renseignez votre ressentit:
 					</legend>
-					{smileys.map(smiley => (
-						<div>
-							<input
-								id={`radio-${smiley.value}`}
-								className={fr.cx('fr-sr-only')}
-								type="radio"
-								name={name}
-								key={smiley.value}
-								onClick={() => {
-									setSmileySelected(smiley.value);
-								}}
-							/>
-							<label
-								htmlFor={`radio-${smiley.value}`}
-								className={cx(classes.smileyInput)}
-							>
-								<Image
-									alt={smiley.alt}
-									src={
-										smileySelected === smiley.value
-											? smiley.imgSelected
-											: smiley.img
-									}
-									width={38}
-									height={38}
+					<ul>
+						{smileys.map(smiley => (
+							<li key={smiley.value}>
+								<input
+									id={`radio-${smiley.value}`}
+									className={fr.cx('fr-sr-only')}
+									type="radio"
+									name={name}
+									onClick={() => {
+										setSmileySelected(smiley.value);
+									}}
 								/>
-								{smiley.label}
-							</label>
-						</div>
-					))}
+								<label
+									htmlFor={`radio-${smiley.value}`}
+									className={cx(classes.smileyInput)}
+								>
+									<Image
+										alt={smiley.alt}
+										src={
+											smileySelected === smiley.value
+												? smiley.imgSelected
+												: smiley.img
+										}
+										width={38}
+										height={38}
+									/>
+									{smiley.label}
+								</label>
+							</li>
+						))}
+					</ul>
 				</fieldset>
 			</div>
 		</div>
@@ -97,15 +98,21 @@ export const SmileyInput = (props: Props) => {
 
 const useStyles = tss
 	.withName(SmileyInput.name)
-	.withParams()
-	.create(() => ({
+	.withParams<{ nbItems: number }>()
+	.create(({ nbItems }) => ({
 		smileysContainer: {
 			display: 'flex',
-			marginTop: fr.spacing('4v')
+			marginTop: fr.spacing('4v'),
+			['input:checked + label']: {
+				borderColor: fr.colors.decisions.background.flat.blueFrance.default
+			},
+			['input:focus-visible + label']: {
+				outlineOffset: '2px',
+				outline: '2px solid #4D90FE'
+			}
 		},
 		smileyInput: {
 			width: '8rem',
-			marginRight: fr.spacing('6v'),
 			border: `1px solid ${fr.colors.decisions.background.alt.grey.hover}`,
 			padding: fr.spacing('1v'),
 			display: 'flex',
@@ -123,6 +130,12 @@ const useStyles = tss
 			...fr.spacing('margin', {
 				rightLeft: 0
 			}),
-			padding: 0
+			padding: 0,
+			ul: {
+				listStyle: 'none',
+				...fr.spacing('margin', { topBottom: 0, rightLeft: 0 }),
+				paddingLeft: 0,
+				columns: nbItems
+			}
 		}
 	}));
