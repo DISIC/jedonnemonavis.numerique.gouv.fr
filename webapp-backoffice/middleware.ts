2c28c6bbb5b3098c7cee375027e1460346396098
Clément <clement@numericite.eu>
2023-09-26 10:35:54 +0200

    fix: middleware

--- webapp-backoffice/middleware.ts
+++ webapp-backoffice/middleware.ts
@@ -3,12 +3,22 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
+const PUBLIC_FILE = /\.(.*)$/;
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
 	const token = await getToken({
 		req: request,
 		secret: process.env.JWT_SECRET
 	});
+
+	if (
+		request.nextUrl.pathname.startsWith('/_next') || // exclude Next.js internals
+		request.nextUrl.pathname.startsWith('/api') || //  exclude all API routes
+		request.nextUrl.pathname.startsWith('/static') || // exclude static files
+		PUBLIC_FILE.test(request.nextUrl.pathname) // exclude all files in the public folder
+	)
+		return NextResponse.next();
 
 	if (request.nextUrl.pathname.startsWith('/administration') && !token) {
 		return NextResponse.redirect(new URL('/login', request.url));
@@ -21,8 +31,3 @@ export async function middleware(request: NextRequest) {
 		);
 	}
 }
-
-// See "Matching Paths" below to learn more
-export const config = {
-	matcher: ['/']
-};
