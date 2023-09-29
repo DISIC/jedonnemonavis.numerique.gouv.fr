370423ab7d8f09eb1132c774b72328c8f73ccf7f
Clément Lelong <clement@numericite.eu>
2023-09-29 16:06:43 +0200

    feat: traduction button design & route refactoring

---
+++ webapp-form/components/global/LanguageSelector.tsx
@@ -0,0 +1,76 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import { memo } from "react";
+import { tss } from "tss-react/dsfr";
+
+export type Language = "fr" | "en";
+
+type Props = {
+  lang: Language;
+  setLang: (lang: Language) => void;
+};
+
+const fullNameByLang: Record<Language, string> = {
+  fr: "Français",
+  en: "English",
+};
+
+const languages: Language[] = ["fr", "en"];
+
+/**
+ * The button controlling the component must specify 2 attributes
+ * - "aria-controls": "translate-select",
+ * - "aria-expanded": false,
+ */
+export const LanguageSelector = memo((props: Props) => {
+  const { lang, setLang } = props;
+
+  const { cx, classes } = useStyles();
+
+  return (
+    <>
+      <div className={classes.root}>
+        {" "}
+        <span className={classes.langShort}>{lang}</span>
+        <span className={fr.cx("fr-hidden-lg")}>
+          {" "}
+          -{fullNameByLang[lang]}
+        </span>{" "}
+      </div>
+      <div
+        className={cx(fr.cx("fr-collapse", "fr-menu"), classes.menuLanguage)}
+        id="translate-select"
+      >
+        <ul className={fr.cx("fr-menu__list")}>
+          {languages.map((lang_i) => (
+            <li key={lang_i}>
+              <a
+                className={fr.cx("fr-translate__language", "fr-nav__link")}
+                href="#"
+                aria-current={lang_i === lang ? "true" : undefined}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setLang(lang_i);
+                }}
+              >
+                <span className={classes.langShort}>{lang_i}</span>
+                &nbsp;-&nbsp;{fullNameByLang[lang_i]}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+});
+
+const useStyles = tss.withName({ LanguageSelector }).create({
+  root: {
+    display: "inline-flex",
+  },
+  menuLanguage: {
+    right: 0,
+  },
+  langShort: {
+    textTransform: "uppercase",
+  },
+});
---
+++ webapp-form/pages/[id].tsx
@@ -0,0 +1,121 @@
+import { FormFirstBlock } from "@/components/form/layouts/FormFirstBlock";
+import { FormSecondBlock } from "@/components/form/layouts/FormSecondBlock";
+import { Opinion, Product } from "@/utils/types";
+import { fr } from "@codegouvfr/react-dsfr";
+import { useState } from "react";
+import { tss } from "tss-react/dsfr";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+import { GetStaticPaths } from "next/types";
+
+export default function JDMAForm() {
+  const { classes, cx } = useStyles();
+  const router = useRouter();
+
+  const { t } = useTranslation("common");
+
+  const product: Product = {
+    title: "1000J BLUES - AUTO DEPISTAGE DE LA DEPRESSION POST PARTUM",
+  };
+
+  const [opinion, setOpinion] = useState<Opinion>({
+    satisfaction: undefined,
+    comprehension: undefined,
+    easy: undefined,
+    difficulties: [],
+    difficulties_verbatim: undefined,
+    help: [],
+    help_verbatim: undefined,
+    verbatim: undefined,
+  });
+
+  return (
+    <div>
+      <div className={classes.blueSection}>
+        {opinion.satisfaction ? (
+          <h1>
+            {t("second_block.title")}
+            <br />
+            {t("second_block.subtitle")}
+          </h1>
+        ) : (
+          <h1>{t("first_block.title")}</h1>
+        )}
+      </div>
+      <div className={fr.cx("fr-container--fluid", "fr-container")}>
+        <div className={fr.cx("fr-grid-row", "fr-grid-row--center")}>
+          <div className={fr.cx("fr-col-12", "fr-col-lg-8")}>
+            <div className={cx(classes.formSection)}>
+              {opinion.satisfaction ? (
+                <FormSecondBlock
+                  opinion={opinion}
+                  onSubmit={(result) => setOpinion({ ...result })}
+                />
+              ) : (
+                <FormFirstBlock
+                  opinion={opinion}
+                  product={product}
+                  onSubmit={(tmpOpinion) => {
+                    setOpinion({ ...tmpOpinion });
+                  }}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: "blocking", //indicates the type of fallback
+  };
+};
+
+const blueSectionPxHeight = 200;
+const useStyles = tss
+  .withName(JDMAForm.name)
+  .withParams()
+  .create(() => ({
+    blueSection: {
+      backgroundColor: fr.colors.decisions.background.alt.blueFrance.default,
+      ...fr.spacing("padding", { topBottom: "6v", rightLeft: "10v" }),
+      h1: {
+        textAlign: "center",
+        fontSize: "2.5rem",
+        margin: 0,
+        color: fr.colors.decisions.background.flat.blueFrance.default,
+        [fr.breakpoints.up("md")]: {
+          display: "none",
+        },
+      },
+      [fr.breakpoints.up("md")]: {
+        height: `${blueSectionPxHeight}px`,
+      },
+    },
+    formSection: {
+      backgroundColor: fr.colors.decisions.background.default.grey.default,
+      ...fr.spacing("padding", { topBottom: "4v", rightLeft: "4v" }),
+      h1: {
+        textAlign: "center",
+        color: fr.colors.decisions.background.flat.blueFrance.default,
+        ...fr.spacing("margin", { bottom: "8v" }),
+      },
+      [fr.breakpoints.up("md")]: {
+        transform: `translateY(-${blueSectionPxHeight / 2}px)`,
+        ...fr.spacing("padding", { topBottom: "8v", rightLeft: "16v" }),
+      },
+    },
+  }));
+
+export async function getStaticProps({ locale }: { locale: any }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "fr", ["common"])),
+    },
+  };
+}
