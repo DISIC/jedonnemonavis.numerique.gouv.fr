4776d5d53ec97630cd11efb08aa296a453978844
horek <azenoxe@gmail.com>
2023-10-06 15:50:33 +0200

    feat: add new property acronym on entity model

--- webapp-backoffice/prisma/seeds/entities.ts
+++ webapp-backoffice/prisma/seeds/entities.ts
@@ -1,11 +1,14 @@
 export const entities = [
 	{
-		name: "Ministère du Travail, du Plein emploi et de l'Insertion"
+		name: "Ministère du Travail, du Plein emploi et de l'Insertion",
+		acronym: 'MTPEI'
 	},
 	{
-		name: "Ministère de l'Économie, des Finances et de la Relance"
+		name: "Ministère de l'Économie, des Finances et de la Relance",
+		acronym: 'MEFR'
 	},
 	{
-		name: "Ministère de l'Éducation nationale, de la Jeunesse et des Sports"
+		name: "Ministère de l'Éducation nationale, de la Jeunesse et des Sports",
+		acronym: 'MENJS'
 	}
 ];
