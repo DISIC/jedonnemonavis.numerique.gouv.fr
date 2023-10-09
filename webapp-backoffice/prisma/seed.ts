b8e8d6c8fcc93154136ee0f349bc097a7d4053bf
Clément <clement@numericite.eu>
2023-10-09 14:35:26 +0200

    fix: prisma seed

--- webapp-backoffice/prisma/seed.ts
+++ webapp-backoffice/prisma/seed.ts
@@ -12,6 +12,7 @@ import { whiteListedDomains } from './seeds/white-listed-domains';
 import { entities } from './seeds/entities';
 import { getRandomObjectFromArray } from '../utils/tools';
 import { buttons } from './seeds/buttons';
+import { Domain } from 'domain';
 
 const prisma = new PrismaClient();
 
@@ -42,60 +43,59 @@ async function main() {
 		);
 	});
 
-	products.forEach((product, index) => {
-		const randomEntity = getRandomObjectFromArray(entities);
-		promisesProducts.push(
-			prisma.product.create({
-				data: {
-					...product,
-					entity: {
-						connect: {
-							name: (randomEntity as Entity).name
-						}
-					},
-					buttons: {
-						create: buttons.map(b => ({
-							...b,
-							product_id: b.product_id
-						})) as Button[]
-					},
-					accessRights: {
-						create: {
-							user_email: users.filter(u => u.active)[index % 2].email
+	Promise.all(promisesUsersAndEntities).then(usersAndEntitiesResponses => {
+		products.forEach((product, index) => {
+			const randomEntity = getRandomObjectFromArray(entities) as Entity;
+			promisesProducts.push(
+				prisma.product.create({
+					data: {
+						...product,
+						entity: {
+							connect: {
+								name: randomEntity.name
+							}
+						},
+						buttons: {
+							create: buttons.map(b => ({
+								...b,
+								product_id: b.product_id
+							})) as Button[]
+						},
+						accessRights: {
+							create: {
+								user_email: users.filter(u => u.active)[index % 2].email
+							}
 						}
 					}
-				}
-			})
-		);
-	});
-
-	whiteListedDomains.forEach(wld => {
-		promisesWLDs.push(
-			prisma.whiteListedDomain.upsert({
-				where: {
-					domain: wld.domain
-				},
-				update: {},
-				create: {
-					...wld
-				}
-			})
-		);
-	});
-
-	Promise.all([
-		...promisesUsersAndEntities,
-		...promisesProducts,
-		...promisesWLDs
-	]).then(responses => {
-		let log: { [key: string]: User | Product | Entity | string } = {};
-		responses.forEach((r, i) => {
-			if ('email' in r) log[`${i}] user added`] = r.email;
-			if ('domain' in r) log[`${i}] domain added : `] = r.domain;
-			if ('title' in r) log[`product ${r.title}`] = r;
-			if ('name' in r) log[`owner ${r.name}`] = r;
+				})
+			);
 		});
-		console.log(log);
+
+		whiteListedDomains.forEach(wld => {
+			promisesWLDs.push(
+				prisma.whiteListedDomain.upsert({
+					where: {
+						domain: wld.domain
+					},
+					update: {},
+					create: {
+						...wld
+					}
+				})
+			);
+		});
+
+		Promise.all([...promisesProducts, ...promisesWLDs]).then(responses => {
+			let log: { [key: string]: User | Product | Entity | Domain | string } =
+				{};
+			usersAndEntitiesResponses.concat(responses as any).forEach((r, i) => {
+				if ('email' in r) log[`${i}] user added`] = r.email;
+				if ('domain' in r) log[`${i}] domain added : `] = r.domain as Domain;
+				if ('title' in r) log[`product ${r.title}`] = r;
+				if ('name' in r) log[`owner ${r.name}`] = r;
+			});
+			console.log(log);
+		});
 	});
 }
 main()
