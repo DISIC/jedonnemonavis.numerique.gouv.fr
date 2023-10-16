c4b56e18359ad95c12a73c793bd8b6b5330f6a16
Clément <clement@numericite.eu>
2023-10-16 18:04:42 +0200

    fix: need of baseUrl from trpc

--- webapp-backoffice/src/utils/trpc.ts
+++ webapp-backoffice/src/utils/trpc.ts
@@ -2,9 +2,7 @@ import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../server/routers/root';
 import SuperJSON from 'superjson';
-function getBaseUrl() {
-	return process.env.NEXTAUTH_URL;
-}
+
 export const trpc = createTRPCNext<AppRouter>({
 	config(opts) {
 		return {
@@ -15,7 +13,7 @@ export const trpc = createTRPCNext<AppRouter>({
 					 * If you want to use SSR, you need to use the server's full URL
 					 * @link https://trpc.io/docs/ssr
 					 **/
-					url: `${getBaseUrl()}/api/trpc`,
+					url: `/api/trpc`,
 					// You can pass any HTTP headers you wish here
 					async headers() {
 						return {
