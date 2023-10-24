c09add78aaa4c5bf96aec52be90dd60940219b4b
horek <azenoxe@gmail.com>
2023-10-24 17:24:14 +0200

    fix: cleanup unused imports

--- webapp-backoffice/src/pages/administration/dashboard/product/[id]/stats.tsx
+++ webapp-backoffice/src/pages/administration/dashboard/product/[id]/stats.tsx
@@ -1,14 +1,7 @@
 import ProductLayout from '@/src/layouts/Product/ProductLayout';
 import { getServerSideProps } from '.';
 import { Product } from '@prisma/client';
-import dynamic from 'next/dynamic';
-import { fr } from '@codegouvfr/react-dsfr';
-import SatisfactionComponent from '@/src/components/dashboard/Stats/Satisfaction';
 import SatisfactionStats from '@/src/components/dashboard/Stats/Satisfaction';
-
-const BarChart = dynamic(() => import('@/src/components/chart/BarChart'), {
-	ssr: false
-});
 
 interface Props {
 	product: Product;
