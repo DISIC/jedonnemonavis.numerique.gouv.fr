b66a33b32d9b2cf644dc8934503973d58557c7bd
Clément <clement@numericite.eu>
2024-03-21 15:31:25 +0100

    fix: reset alert on change modal

--- webapp-backoffice/src/components/dashboard/Entity/EntityRightsModal.tsx
+++ webapp-backoffice/src/components/dashboard/Entity/EntityRightsModal.tsx
@@ -103,6 +103,7 @@ const EntityRightsModal = (props: Props) => {
 
 	const handleModalClose = () => {
 		modal.close();
+		setActionType(null);
 		onClose();
 	};
 
@@ -306,6 +307,7 @@ const EntityRightsModal = (props: Props) => {
 
 	useEffect(() => {
 		setUserAddEmail('');
+		setActionType(null);
 	}, [entity]);
 
 	return (
