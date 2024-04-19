094c72198bbfc53fb447030e021382b122e2d32c
Clément <clement@numericite.eu>
2024-04-19 17:04:20 +0200

    fix: remove open-api from webapp-form

--- webapp-form/src/server/routers/root.ts
+++ webapp-form/src/server/routers/root.ts
@@ -1,10 +1,8 @@
 import { router } from "@/src/server/trpc";
 import { reviewRouter } from "./review";
-import { productRouter } from "./product";
 
 export const appRouter = router({
   review: reviewRouter,
-  product: productRouter,
 });
 
 export type AppRouter = typeof appRouter;
--- webapp-form/src/server/trpc.ts
+++ webapp-form/src/server/trpc.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from "@prisma/client";
 import { inferAsyncReturnType, initTRPC } from "@trpc/server";
 import SuperJSON from "superjson";
 import { ZodError } from "zod";
-import { OpenApiMeta } from "trpc-openapi";
 import { Client as ElkClient } from "@elastic/elasticsearch";
 
 // Create context with Prisma and NextAuth session
@@ -31,27 +30,22 @@ export const createContext = async () => {
 
 export type Context = inferAsyncReturnType<typeof createContext>;
 
-const t = initTRPC
-  .context<Context>()
-  .meta<OpenApiMeta>()
-  .create({
-    transformer: SuperJSON,
-    errorFormatter({ shape, error }) {
-      return {
-        ...shape,
-        data: {
-          ...shape.data,
-          zodError:
-            error.cause instanceof ZodError
-              ? error.cause.flatten()
-              : error.cause,
-          cause: {
-            ...error.cause,
-          },
+const t = initTRPC.context<Context>().create({
+  transformer: SuperJSON,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : error.cause,
+        cause: {
+          ...error.cause,
         },
-      };
-    },
-  });
+      },
+    };
+  },
+});
 
 // Base router and middleware helpers
 export const router = t.router;
