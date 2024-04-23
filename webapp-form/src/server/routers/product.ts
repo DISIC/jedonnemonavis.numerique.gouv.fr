6813930b68fc0d1ccd3097bfbd92420238368a28
Clément Lelong <clement@numericite.eu>
2024-04-23 15:35:49 +0200

    fix: recursive warning

--- webapp-form/src/server/routers/product.ts
+++ webapp-form/src/server/routers/product.ts
@@ -1,27 +1,27 @@
-import { number, string, z } from 'zod';
-import { router, publicProcedure } from '@/src/server/trpc';
-import {  ProductWithPartialRelationsSchema } from '@/prisma/generated/zod';
-import { TRPCError } from '@trpc/server';
+import { ProductSchema } from "@/prisma/generated/zod";
+import { publicProcedure, router } from "@/src/server/trpc";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const productRouter = router({
   getById: publicProcedure
-    .meta({ openapi: { method: 'GET', path: '/product/{id}' } })
+    .meta({ openapi: { method: "GET", path: "/product/{id}" } })
     .input(z.object({ id: z.number() }))
-    .output(z.object({ data: ProductWithPartialRelationsSchema }))
+    .output(z.object({ data: ProductSchema }))
     .query(async ({ ctx, input }) => {
       const product = await ctx.prisma.product.findUnique({
         where: {
-          id: input.id
+          id: input.id,
         },
         include: {
-          buttons: true
-        }
+          buttons: true,
+        },
       });
 
       if (!product)
         throw new TRPCError({
-          code: 'NOT_FOUND',
-          message: 'Product not found'
+          code: "NOT_FOUND",
+          message: "Product not found",
         });
 
       return { data: product };
