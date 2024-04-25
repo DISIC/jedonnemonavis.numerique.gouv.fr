9c3d764740fdc4dc7dde0c669dbe0d9eb47744bc
Clément <clement@numericite.eu>
2024-04-25 14:47:19 +0200

    fix: remove unecessary radio in arrayInput

--- webapp-form/src/utils/form.tsx
+++ webapp-form/src/utils/form.tsx
@@ -162,11 +162,6 @@ export const secondSectionA: FormField[] = [
         value: 23,
         intention: "very_good",
       },
-      {
-        label: "fields.contact_satisfaction.options.5.label",
-        value: 24,
-        intention: "neutral",
-      },
     ],
     needed: [9, 10, 11, 12],
     excluded: [16],
@@ -210,5 +205,5 @@ export const steps_B: Step[] = [
     name: "steps.B_2.name",
     section: firstSectionA.concat(secondSectionA, thirdSectionA),
     button: "steps.B_2.button",
-  }
+  },
 ];
