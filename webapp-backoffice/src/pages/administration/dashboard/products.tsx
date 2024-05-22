fcf49efe57540b3785c48b80b597da353ec5ca2f
FredericNumericite <frederic@numericite.eu>
2024-05-22 12:11:58 +0200

    fix: add checks for loading component

--- webapp-backoffice/src/pages/administration/dashboard/products.tsx
+++ webapp-backoffice/src/pages/administration/dashboard/products.tsx
@@ -18,7 +18,7 @@ import { Autocomplete } from '@mui/material';
 import { Entity } from '@prisma/client';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { tss } from 'tss-react/dsfr';
 
 const product_modal = createModal({
@@ -102,316 +102,328 @@ const DashBoard = () => {
 			: 'Services | Je donne mon avis';
 	};
 
-	return (
-		<>
-			<Head>
-				<title>{headTitle()}</title>
-				<meta name="description" content={headTitle()} />
-			</Head>
-			<ProductModal
-				modal={product_modal}
-				onSubmit={() => {
-					setSearch('');
-					if (filters.filter === 'created_at') {
-						updateFilters({ ...filters, validatedSearch: '' });
-					} else {
-						updateFilters({ ...filters, filter: 'created_at' });
-					}
-				}}
-			/>
-			{products.length === 0 &&
-			filters.filterEntity.length === 0 &&
-			filters.validatedSearch === '' ? (
+	const loadModalAndHead = () => {
+		return (
+			<>
+				<Head>
+					<title>{headTitle()}</title>
+					<meta name="description" content={headTitle()} />
+				</Head>
+				<ProductModal
+					modal={product_modal}
+					onSubmit={() => {
+						setSearch('');
+						if (filters.filter === 'created_at') {
+							updateFilters({ ...filters, validatedSearch: '' });
+						} else {
+							updateFilters({ ...filters, filter: 'created_at' });
+						}
+					}}
+				/>
+			</>
+		);
+	};
+
+	if (
+		products.length === 0 &&
+		filters.filterEntity.length === 0 &&
+		filters.validatedSearch === '' &&
+		!isLoadingProducts &&
+		!isRefetchingProducts
+	) {
+		return (
+			<>
+				{loadModalAndHead()}
 				<ProductEmptyState
 					onButtonClick={() => {
 						product_modal.open();
 					}}
 				/>
-			) : (
-				<div className={fr.cx('fr-container', 'fr-py-6w')}>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<div className={fr.cx('fr-container', 'fr-py-6w')}>
+				<div
+					className={fr.cx('fr-grid-row', 'fr-grid-row--gutters', 'fr-mb-3w')}
+				>
+					<div className={fr.cx('fr-col-12', 'fr-col-md-4')}>
+						<h1 className={fr.cx('fr-mb-0')}>
+							{session?.user?.role !== 'admin' ? 'Mes services' : 'Services'}
+						</h1>
+					</div>
 					<div
-						className={fr.cx('fr-grid-row', 'fr-grid-row--gutters', 'fr-mb-3w')}
+						className={cx(
+							fr.cx('fr-col-12', 'fr-col-md-8'),
+							classes.buttonContainer
+						)}
 					>
-						<div className={fr.cx('fr-col-12', 'fr-col-md-4')}>
-							<h1 className={fr.cx('fr-mb-0')}>
-								{session?.user?.role !== 'admin' ? 'Mes services' : 'Services'}
-							</h1>
+						<Button
+							priority="secondary"
+							iconId="fr-icon-add-circle-line"
+							iconPosition="right"
+							type="button"
+							nativeButtonProps={product_modal.buttonProps}
+						>
+							Ajouter un nouveau service
+						</Button>
+					</div>
+				</div>
+				<div className={fr.cx('fr-grid-row', 'fr-grid-row--gutters')}>
+					<div className={fr.cx('fr-col-12', 'fr-col-md-3')}>
+						<Select
+							label="Trier Par"
+							nativeSelectProps={{
+								name: 'my-select',
+								value: filters.filter,
+								onChange: event =>
+									updateFilters({ ...filters, filter: event.target.value })
+							}}
+						>
+							<option value="title:asc">Nom A à Z</option>
+							<option value="entity.name:asc">Organisation A à Z</option>
+							<option value="created_at:desc">Date de création</option>
+							<option value="updated_at:desc">Date de mise à jour</option>
+						</Select>
+					</div>
+					<div className={fr.cx('fr-col-12', 'fr-col-md-4')}>
+						<Autocomplete
+							id="filter-entity"
+							disablePortal
+							sx={{ width: '100%' }}
+							options={entities
+								.map(entity => ({
+									label: `${entity.name} (${entity.acronym})`,
+									value: entity.id
+								}))
+								.filter(
+									entity =>
+										!filters.filterEntity.some(
+											filter => filter.value === entity.value
+										)
+								)}
+							onChange={(_, option) => {
+								if (option)
+									updateFilters({
+										...filters,
+										filterEntity: [...filters.filterEntity, option] ?? null,
+										currentPage: 1
+									});
+							}}
+							noOptionsText="Aucune organisation trouvée"
+							inputValue={inputValue}
+							onInputChange={(event, newInputValue) => {
+								setInputValue(newInputValue);
+							}}
+							renderInput={params => (
+								<div ref={params.InputProps.ref}>
+									<label htmlFor="filter-entity" className="fr-label">
+										Filtrer par organisation
+									</label>
+									<input
+										{...params.inputProps}
+										className={params.inputProps.className + ' fr-input'}
+										placeholder="Sélectionner une option"
+										type="search"
+									/>
+								</div>
+							)}
+						/>
+					</div>
+					<div className={fr.cx('fr-col-12', 'fr-col-md-5', 'fr-col--bottom')}>
+						<form
+							className={cx(classes.searchForm)}
+							onSubmit={e => {
+								e.preventDefault();
+								updateFilters({
+									...filters,
+									currentPage: 1,
+									validatedSearch: search
+								});
+							}}
+						>
+							<div role="search" className={fr.cx('fr-search-bar')}>
+								<Input
+									label="Rechercher un service"
+									hideLabel
+									nativeInputProps={{
+										placeholder: 'Rechercher un service',
+										type: 'search',
+										value: search,
+										onChange: event => {
+											if (!event.target.value) {
+												updateFilters({
+													...filters,
+													currentPage: 1,
+													validatedSearch: ''
+												});
+											}
+											setSearch(event.target.value);
+										}
+									}}
+								/>
+								<Button
+									priority="primary"
+									type="submit"
+									iconId="ri-search-2-line"
+									iconPosition="left"
+								>
+									Rechercher
+								</Button>
+							</div>
+						</form>
+					</div>
+					{session?.user.role !== 'user' && (
+						<div
+							className={fr.cx(
+								'fr-col-12',
+								'fr-mt-4w',
+								nbPages > 1 ? 'fr-mb-2w' : 'fr-mb-0',
+								'fr-py-0'
+							)}
+						>
+							<Checkbox
+								className={fr.cx('fr-mb-0')}
+								style={{ userSelect: 'none' }}
+								options={[
+									{
+										label: 'Afficher uniquement mes favoris',
+										nativeInputProps: {
+											name: 'favorites-products',
+											checked: filters.filterOnlyFavorites,
+											onChange: e => {
+												updateFilters({
+													...filters,
+													currentPage: 1,
+													filterOnlyFavorites: e.target.checked
+												});
+											}
+										}
+									}
+								]}
+							/>
+						</div>
+					)}
+					<div className={fr.cx('fr-col-12', 'fr-col-md-12', 'fr-col--bottom')}>
+						{filters.filterEntity.map(entity => (
+							<Tag
+								dismissible
+								className={cx(classes.tagFilter)}
+								nativeButtonProps={{
+									onClick: () => {
+										updateFilters({
+											...filters,
+											filterEntity: filters.filterEntity.filter(
+												e => e.value !== entity.value
+											)
+										});
+										setInputValue('');
+									}
+								}}
+							>
+								<p>{entity.label}</p>
+							</Tag>
+						))}
+					</div>
+				</div>
+				{isLoadingProducts || isLoadingEntities || isLoadingFavorites ? (
+					<div className={fr.cx('fr-py-20v', 'fr-mt-4w')}>
+						<Loader />
+					</div>
+				) : (
+					<div>
+						<div className={fr.cx('fr-col-8', 'fr-pt-3w')}>
+							{nbPages > 1 && (
+								<span className={fr.cx('fr-ml-0')}>
+									Services de{' '}
+									<span className={cx(classes.boldText)}>
+										{numberPerPage * (filters.currentPage - 1) + 1}
+									</span>{' '}
+									à{' '}
+									<span className={cx(classes.boldText)}>
+										{numberPerPage * (filters.currentPage - 1) +
+											products.length}
+									</span>{' '}
+									sur{' '}
+									<span className={cx(classes.boldText)}>
+										{productsResult.metadata.count}
+									</span>
+								</span>
+							)}
 						</div>
 						<div
 							className={cx(
-								fr.cx('fr-col-12', 'fr-col-md-8'),
-								classes.buttonContainer
+								products.length === 0 ? classes.productsContainer : ''
 							)}
 						>
-							<Button
-								priority="secondary"
-								iconId="fr-icon-add-circle-line"
-								iconPosition="right"
-								type="button"
-								nativeButtonProps={product_modal.buttonProps}
-							>
-								Ajouter un nouveau service
-							</Button>
-						</div>
-					</div>
-					<div className={fr.cx('fr-grid-row', 'fr-grid-row--gutters')}>
-						<div className={fr.cx('fr-col-12', 'fr-col-md-3')}>
-							<Select
-								label="Trier Par"
-								nativeSelectProps={{
-									name: 'my-select',
-									value: filters.filter,
-									onChange: event =>
-										updateFilters({ ...filters, filter: event.target.value })
-								}}
-							>
-								<option value="title:asc">Nom A à Z</option>
-								<option value="entity.name:asc">Organisation A à Z</option>
-								<option value="created_at:desc">Date de création</option>
-								<option value="updated_at:desc">Date de mise à jour</option>
-							</Select>
-						</div>
-						<div className={fr.cx('fr-col-12', 'fr-col-md-4')}>
-							<Autocomplete
-								id="filter-entity"
-								disablePortal
-								sx={{ width: '100%' }}
-								options={entities
-									.map(entity => ({
-										label: `${entity.name} (${entity.acronym})`,
-										value: entity.id
-									}))
-									.filter(
-										entity =>
-											!filters.filterEntity.some(
-												filter => filter.value === entity.value
-											)
-									)}
-								onChange={(_, option) => {
-									if (option)
-										updateFilters({
-											...filters,
-											filterEntity: [...filters.filterEntity, option] ?? null,
-											currentPage: 1
-										});
-								}}
-								noOptionsText="Aucune organisation trouvée"
-								inputValue={inputValue}
-								onInputChange={(event, newInputValue) => {
-									setInputValue(newInputValue);
-								}}
-								renderInput={params => (
-									<div ref={params.InputProps.ref}>
-										<label htmlFor="filter-entity" className="fr-label">
-											Filtrer par organisation
-										</label>
-										<input
-											{...params.inputProps}
-											className={params.inputProps.className + ' fr-input'}
-											placeholder="Sélectionner une option"
-											type="search"
-										/>
-									</div>
-								)}
-							/>
-						</div>
-						<div
-							className={fr.cx('fr-col-12', 'fr-col-md-5', 'fr-col--bottom')}
-						>
-							<form
-								className={cx(classes.searchForm)}
-								onSubmit={e => {
-									e.preventDefault();
-									updateFilters({
-										...filters,
-										currentPage: 1,
-										validatedSearch: search
-									});
-								}}
-							>
-								<div role="search" className={fr.cx('fr-search-bar')}>
-									<Input
-										label="Rechercher un service"
-										hideLabel
-										nativeInputProps={{
-											placeholder: 'Rechercher un service',
-											type: 'search',
-											value: search,
-											onChange: event => {
-												if (!event.target.value) {
-													updateFilters({
-														...filters,
-														currentPage: 1,
-														validatedSearch: ''
-													});
-												}
-												setSearch(event.target.value);
-											}
-										}}
-									/>
-									<Button
-										priority="primary"
-										type="submit"
-										iconId="ri-search-2-line"
-										iconPosition="left"
-									>
-										Rechercher
-									</Button>
+							{isRefetchingProducts ? (
+								<div className={fr.cx('fr-py-20v', 'fr-mt-4w')}>
+									<Loader />
 								</div>
-							</form>
+							) : (
+								products.map((product, index) => (
+									<ProductCard
+										product={product}
+										userId={parseInt(session?.user?.id as string)}
+										entity={
+											entities.find(
+												entity => product.entity_id === entity.id
+											) as Entity
+										}
+										isFavorite={
+											!!favorites.find(
+												favorite => favorite.product_id === product.id
+											)
+										}
+										key={index}
+									/>
+								))
+							)}
+
+							{products.length === 0 && !isRefetchingProducts && (
+								<div className={fr.cx('fr-grid-row', 'fr-grid-row--center')}>
+									<div
+										className={cx(
+											fr.cx('fr-col-12', 'fr-col-md-5', 'fr-mt-30v'),
+											classes.textContainer
+										)}
+										role="status"
+									>
+										<p>Aucun service trouvé</p>
+									</div>
+								</div>
+							)}
 						</div>
-						{session?.user.role !== 'user' && (
-							<div
-								className={fr.cx(
-									'fr-col-12',
-									'fr-mt-4w',
-									nbPages > 1 ? 'fr-mb-2w' : 'fr-mb-0',
-									'fr-py-0'
-								)}
-							>
-								<Checkbox
-									className={fr.cx('fr-mb-0')}
-									style={{ userSelect: 'none' }}
-									options={[
-										{
-											label: 'Afficher uniquement mes favoris',
-											nativeInputProps: {
-												name: 'favorites-products',
-												checked: filters.filterOnlyFavorites,
-												onChange: e => {
-													updateFilters({
-														...filters,
-														currentPage: 1,
-														filterOnlyFavorites: e.target.checked
-													});
-												}
-											}
-										}
-									]}
-								/>
-							</div>
-						)}
 						<div
-							className={fr.cx('fr-col-12', 'fr-col-md-12', 'fr-col--bottom')}
+							className={fr.cx(
+								'fr-grid-row--center',
+								'fr-grid-row',
+								'fr-mb-15w'
+							)}
 						>
-							{filters.filterEntity.map(entity => (
-								<Tag
-									dismissible
-									className={cx(classes.tagFilter)}
-									nativeButtonProps={{
-										onClick: () => {
-											updateFilters({
-												...filters,
-												filterEntity: filters.filterEntity.filter(
-													e => e.value !== entity.value
-												)
-											});
-											setInputValue('');
-										}
-									}}
-								>
-									<p>{entity.label}</p>
-								</Tag>
-							))}
+							{nbPages > 1 && (
+								<Pagination
+									showFirstLast
+									count={nbPages}
+									defaultPage={filters.currentPage}
+									getPageLinkProps={pageNumber => ({
+										onClick: event => {
+											event.preventDefault();
+											handlePageChange(pageNumber);
+										},
+										href: '#',
+										classes: { link: fr.cx('fr-pagination__link') },
+										key: `pagination-link-product-${pageNumber}`
+									})}
+									className={fr.cx('fr-mt-1w')}
+								/>
+							)}
 						</div>
 					</div>
-					{isLoadingProducts || isLoadingEntities || isLoadingFavorites ? (
-						<div className={fr.cx('fr-py-20v', 'fr-mt-4w')}>
-							<Loader />
-						</div>
-					) : (
-						<div>
-							<div className={fr.cx('fr-col-8', 'fr-pt-3w')}>
-								{nbPages > 1 && (
-									<span className={fr.cx('fr-ml-0')}>
-										Services de{' '}
-										<span className={cx(classes.boldText)}>
-											{numberPerPage * (filters.currentPage - 1) + 1}
-										</span>{' '}
-										à{' '}
-										<span className={cx(classes.boldText)}>
-											{numberPerPage * (filters.currentPage - 1) +
-												products.length}
-										</span>{' '}
-										sur{' '}
-										<span className={cx(classes.boldText)}>
-											{productsResult.metadata.count}
-										</span>
-									</span>
-								)}
-							</div>
-							<div
-								className={cx(
-									products.length === 0 ? classes.productsContainer : ''
-								)}
-							>
-								{isRefetchingProducts ? (
-									<div className={fr.cx('fr-py-20v', 'fr-mt-4w')}>
-										<Loader />
-									</div>
-								) : (
-									products.map((product, index) => (
-										<ProductCard
-											product={product}
-											userId={parseInt(session?.user?.id as string)}
-											entity={
-												entities.find(
-													entity => product.entity_id === entity.id
-												) as Entity
-											}
-											isFavorite={
-												!!favorites.find(
-													favorite => favorite.product_id === product.id
-												)
-											}
-											key={index}
-										/>
-									))
-								)}
-
-								{products.length === 0 && !isRefetchingProducts && (
-									<div className={fr.cx('fr-grid-row', 'fr-grid-row--center')}>
-										<div
-											className={cx(
-												fr.cx('fr-col-12', 'fr-col-md-5', 'fr-mt-30v'),
-												classes.textContainer
-											)}
-											role="status"
-										>
-											<p>Aucun service trouvé</p>
-										</div>
-									</div>
-								)}
-							</div>
-							<div
-								className={fr.cx(
-									'fr-grid-row--center',
-									'fr-grid-row',
-									'fr-mb-15w'
-								)}
-							>
-								{nbPages > 1 && (
-									<Pagination
-										showFirstLast
-										count={nbPages}
-										defaultPage={filters.currentPage}
-										getPageLinkProps={pageNumber => ({
-											onClick: event => {
-												event.preventDefault();
-												handlePageChange(pageNumber);
-											},
-											href: '#',
-											classes: { link: fr.cx('fr-pagination__link') },
-											key: `pagination-link-product-${pageNumber}`
-										})}
-										className={fr.cx('fr-mt-1w')}
-									/>
-								)}
-							</div>
-						</div>
-					)}
-				</div>
-			)}
+				)}
+			</div>
 		</>
 	);
 };
