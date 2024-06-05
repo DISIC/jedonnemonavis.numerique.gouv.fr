78cf1433e5c6d965631b40b64f5b5eacddaf4e0c
FredericNumericite <frederic@numericite.eu>
2024-06-05 17:19:22 +0200

    feat: add new kpi Card component for stats

---
+++ webapp-backoffice/src/components/dashboard/Stats/KPICard.tsx
@@ -0,0 +1,88 @@
+import { getStatsColor, getStatsIcon } from '@/src/utils/stats';
+import { fr } from '@codegouvfr/react-dsfr';
+import Link from 'next/link';
+import { tss } from 'tss-react/dsfr';
+
+type Props = {
+	mainNumber: number;
+	title: string;
+	description?: string;
+	link?: string;
+	linkName?: string;
+};
+
+const KPICard = ({ mainNumber, title, description, link, linkName }: Props) => {
+	const { classes, cx } = useStyles({ link });
+	const parseMainNumber = () => {
+		return mainNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+	};
+
+	return (
+		<div className={cx(classes.kpiCardWrapper)}>
+			<h2>{parseMainNumber()}</h2>
+			<h6 className={cx(classes.title, link ? cx(classes.blueText) : '')}>
+				{title}
+			</h6>
+			<div className={cx(classes.description)}>
+				{description && <p>{description}</p>}
+			</div>
+			{link && linkName && (
+				<Link href={link} className={cx(classes.blueText, classes.linkLabel)}>
+					{linkName}{' '}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1.2em"
+						height="1.2em"
+						viewBox="0 0 28 28"
+					>
+						<path
+							fill="currentColor"
+							d="M15.705 4.289a1 1 0 1 0-1.406 1.422L21.675 13H4a1 1 0 1 0 0 2h17.673l-7.374 7.286a1 1 0 0 0 1.406 1.423l8.927-8.821a1.25 1.25 0 0 0 0-1.778z"
+						/>
+					</svg>
+				</Link>
+			)}
+		</div>
+	);
+};
+
+const useStyles = tss
+	.withName(KPICard.name)
+	.withParams<{ link: string | undefined }>()
+	.create(({ link }) => ({
+		kpiCardWrapper: {
+			background: 'white',
+			border: '1px solid lightgray',
+			padding: '2rem 1.5rem',
+			width: '33%',
+			display: 'flex',
+			alignItems: 'center',
+			flexDirection: 'column',
+			borderBottom: `5px solid ${link ? fr.colors.decisions.text.title.blueFrance.default : '#3A3A3A'}`
+		},
+		title: {
+			...fr.typography[19].style,
+			marginBottom: '0.25rem'
+		},
+		blueText: {
+			color: fr.colors.decisions.text.title.blueFrance.default
+		},
+		linkLabel: {
+			display: 'flex',
+			alignItems: 'center',
+			gap: '0.5rem',
+			...fr.typography[18].style,
+			margin: 0,
+			background: 'none',
+			fontWeight: 500,
+			['&:hover']: {
+				textDecoration: 'underline'
+			}
+		},
+		description: {
+			minHeight: 50,
+			textAlign: 'center'
+		}
+	}));
+
+export default KPICard;
