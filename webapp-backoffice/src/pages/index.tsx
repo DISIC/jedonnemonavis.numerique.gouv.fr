92812179fcdb2687753ad09c443214e9c85f58b9
Clément <clement@numericite.eu>
2024-06-14 12:21:54 +0200

    fix: hack to route next images from quote comopnent of react-dsfr

--- webapp-backoffice/src/pages/index.tsx
+++ webapp-backoffice/src/pages/index.tsx
@@ -68,14 +68,14 @@ export default function Home() {
 
 	const references: Reference[] = [
 		{
-			image_path: '/assets/temoignage_1.jpeg',
+			image_path: '/_next/image?url=%2Fassets%2Ftemoignage_1.jpeg&w=640&q=75',
 			description:
 				'Le bouton je donne mon avis, que nous avons installé pour récolter les avis des usagers de la démarche de paiement des impôts en ligne, nous a fournis les pistes pour augmenter le taux de satisfaction de 20% en un an !',
 			author: 'Fabienne D.',
 			job_title: 'Responsable projet à la DGFIP'
 		},
 		{
-			image_path: '/assets/temoignage_2.jpeg',
+			image_path: '/_next/image?url=%2Fassets%2Ftemoignage_2.jpeg&w=640&q=75',
 			description:
 				'Cela nous a pris moins de 2h pour configurer et installer le bouton Je donne mon avis. Le code est extrêmement simple et la procédure bien indiquée.',
 			author: 'Mathilde P.',
