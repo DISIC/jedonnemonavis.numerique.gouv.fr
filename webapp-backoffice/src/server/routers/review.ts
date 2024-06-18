f51068978dc03999cddd16cc9cdd77cc246a2bd3
Clément Lelong <clement@numericite.eu>
2024-06-18 15:23:34 +0200

    fix: remove console logs

--- webapp-backoffice/src/server/routers/review.ts
+++ webapp-backoffice/src/server/routers/review.ts
@@ -99,9 +99,6 @@ export const reviewRouter = router({
 				})
 			]);
 
-			console.log(where);
-			console.log(await ctx.prisma.review.count({ where }));
-
 			return { data: reviews, metadata: { countFiltered, countAll } };
 		}),
 
