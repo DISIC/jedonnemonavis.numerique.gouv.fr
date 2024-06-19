70a77dd7c8f1c7c8aa0a0ebc741e32698f45f7ab
YoannNumericite <y.lebars@numericite.eu>
2024-06-19 17:14:01 +0200

    fix: fix migrations export table

--- webapp-backoffice/prisma/migrations/20240618095137_add_exports_table/migration.sql
+++ webapp-backoffice/prisma/migrations/20240618095137_add_exports_table/migration.sql
@@ -1,9 +1,3 @@
--- DropIndex
-DROP INDEX "Answer_review_id_review_created_at_field_code_idx";
-
--- AlterTable
-ALTER TABLE "Review" ADD COLUMN     "user_id" TEXT;
-
 -- CreateTable
 CREATE TABLE "Exports" (
     "id" SERIAL NOT NULL,
@@ -19,3 +13,51 @@ CREATE UNIQUE INDEX "Exports_user_id_key" ON "Exports"("user_id");
 
 -- AddForeignKey
 ALTER TABLE "Exports" ADD CONSTRAINT "Exports_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+/*
+  Warnings:
+
+  - You are about to drop the `Exports` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Exports" DROP CONSTRAINT "Exports_user_id_fkey";
+
+-- DropTable
+DROP TABLE "Exports";
+
+-- CreateTable
+CREATE TABLE "Export" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" INTEGER NOT NULL,
+    "params" TEXT NOT NULL,
+
+    CONSTRAINT "Export_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Export_user_id_key" ON "Export"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "Export" ADD CONSTRAINT "Export_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+/*
+  Warnings:
+
+  - Added the required column `product_id` to the `Export` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Export_user_id_key";
+
+-- AlterTable
+ALTER TABLE "Export" ADD COLUMN     "link" TEXT,
+ADD COLUMN     "product_id" INTEGER NOT NULL,
+ALTER COLUMN "params" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Export" ADD CONSTRAINT "Export_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AlterTable
+ALTER TABLE "Export" ADD COLUMN     "processed" BOOLEAN NOT NULL DEFAULT false;
--- webapp-backoffice/prisma/migrations/20240618124834_add_processed_to_model_export/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Export" ADD COLUMN     "processed" BOOLEAN NOT NULL DEFAULT false;
