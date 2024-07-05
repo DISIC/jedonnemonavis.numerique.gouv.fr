9d3fed12093af53ef663971be86e26bc58bd3d3b
Clément <clement@numericite.eu>
2024-07-05 09:40:00 +0200

    fix: tally notice

--- webapp-backoffice/src/layouts/PublicLayout.tsx
+++ webapp-backoffice/src/layouts/PublicLayout.tsx
@@ -180,27 +180,14 @@ export default function PublicLayout({ children, light }: PublicLayoutProps) {
 					}}
 					title={
 						<>
-							Migration en cours : certains avis déposés récemment peuvent
-							mettre jusqu'à 24h avant de s'afficher (03/07/24, à 15h)
+							Aidez-nous à améliorer cet outil, n'hésitez pas à nous faire part
+							de vos retours depuis{' '}
+							<a href="https://tally.so/r/m6kyyB" target="_blank">
+								ce court formulaire
+							</a>
 						</>
 					}
 				/>
-				// <Notice
-				// 	isClosable
-				// 	onClose={function noRefCheck() {}}
-				// 	style={{
-				// 		marginBottom: '-1rem'
-				// 	}}
-				// 	title={
-				// 		<>
-				// 			Aidez-nous à améliorer cet outil, n'hésitez pas à nous faire part
-				// 			de vos retours depuis{' '}
-				// 			<a href="https://tally.so/r/m6kyyB" target="_blank">
-				// 				ce court formulaire
-				// 			</a>
-				// 		</>
-				// 	}
-				// />
 			)}
 			{/* END FOR BETA TESTING */}
 			<main id="main" role="main" tabIndex={-1}>
