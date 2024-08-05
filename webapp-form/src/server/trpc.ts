15e4c19dbe470ed179e66c01ceb4c97d6a13ee9f
Madyo <y.lebars@numericite.eu>
2024-08-05 15:30:27 +0200

    fix: try getting 'x-forwarded-for' header

--- webapp-form/src/server/trpc.ts
+++ webapp-form/src/server/trpc.ts
@@ -55,7 +55,13 @@ const t = initTRPC.context<Context>().create({
 });
 
 const limiter = createTRPCStoreLimiter<typeof t>({
-  fingerprint: (ctx) => {console.log("attempting create review with IP : ", defaultFingerPrint(ctx.req)); return defaultFingerPrint(ctx.req)},
+  fingerprint: (ctx) => {
+    const xForwardedFor = ctx.req.headers['x-forwarded-for'] as string;
+    const ip = xForwardedFor ? xForwardedFor.split(',')[0] : defaultFingerPrint(ctx.req)
+    console.log("default fingerprint : ", defaultFingerPrint(ctx.req));
+    console.log("xForwardedFor : ", xForwardedFor);
+    return ip;
+  },
   windowMs: 60000,
   max: 5,
   onLimit: (retryAfter) => {
