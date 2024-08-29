01217f1bfec6f72435150bd4edacf2011f89731b
Madyo <y.lebars@numericite.eu>
2024-08-29 12:08:19 +0200

    feat: reporting limiter

--- webapp-form/src/server/trpc.ts
+++ webapp-form/src/server/trpc.ts
@@ -8,6 +8,7 @@ import path from "path";
 import SuperJSON from "superjson";
 import { ZodError } from "zod";
 import prisma from "../utils/db";
+import crypto from 'crypto';
 
 // Create context with Prisma and NextAuth session
 export const createContext = async (opts: CreateNextContextOptions) => {
@@ -54,16 +55,40 @@ const t = initTRPC.context<Context>().create({
   },
 });
 
+const hashIp = (ip: string) => {
+  // Hash IP with date until hour, and salt
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const dateHour = `${year}-${month}-${day}-${hour}`;
+  const ipWithDate = `${ip}-${dateHour}`;
+  const hash = crypto.createHash('sha256');
+  hash.update(ipWithDate + process.env.IP_HASH_SALT); 
+  return hash.digest('hex');
+};
+
+const transformIp = (ip: string) => {
+  const parts = ip.split('.');
+  parts[3] = '0';
+  const transformedIp = parts.join('.');
+  return transformedIp;
+}
+
+const extractIdsFromUrl = (url: string) => {
+  const urlObj = new URL(url);
+  const pathParts = urlObj.pathname.split('/');
+  const product_id = pathParts[2];
+  const button_id = urlObj.searchParams.get('button');
+  return [product_id, button_id];
+}
+
 const limiter = createTRPCStoreLimiter<typeof t>({
   fingerprint: (ctx) => {
     const xForwardedFor = ctx.req.headers['x-forwarded-for'] as string;
     const xClientIp = ctx.req.headers['x-client-ip'] as string;
-    console.log('---------------------------------');
-    console.log('x-forwarded-for', xForwardedFor);
-    console.log('x-client-ip', xClientIp);
-    const ip = xClientIp ? xClientIp.split(',')[0] : xForwardedFor ? xForwardedFor.split(',')[0] : '192.168.0.1';
-    console.log('ip', ip);
-    console.log('---------------------------------');
+    const ip = xClientIp ? xClientIp.split(',')[0] : xForwardedFor ? xForwardedFor.split(',')[0] : defaultFingerPrint(ctx.req);
     return ip;
   },
   windowMs: 60000,
@@ -72,8 +97,43 @@ const limiter = createTRPCStoreLimiter<typeof t>({
 
     const xForwardedFor = ctx.req.headers['x-forwarded-for'] as string;
     const xClientIp = ctx.req.headers['x-client-ip'] as string;
-    const ip = xClientIp ? xClientIp.split(',')[0] : xForwardedFor ? xForwardedFor.split(',')[0] : '192.168.0.1';
+    const ip = xClientIp ? xClientIp.split(',')[0] : xForwardedFor ? xForwardedFor.split(',')[0] : defaultFingerPrint(ctx.req);
+    const referer = ctx.req.headers['referer'] || ctx.req.headers['referrer'];
+    const hashedIp = hashIp(ip);
     const currentTime = new Date();
+    const [product_id, button_id] = extractIdsFromUrl(referer as string);
+
+    // check if the hashed ip is already in the database
+    const ipRecord = await prisma.limiterReporting.findFirst({
+      where: {
+        ip_id: hashedIp,
+      },
+    });
+
+    // insert or update database
+    if (ipRecord) {
+      await prisma.limiterReporting.update({
+        where: {
+          id: ipRecord.id,
+        },
+        data: {
+          total_attempts: ipRecord.total_attempts + 1,
+          last_attempt: currentTime,
+        },
+      });
+    } else {
+      await prisma.limiterReporting.create({
+        data: {
+          ip_id: hashedIp,
+          ip_adress: transformIp(ip),
+          product_id: parseInt(product_id ?? '0'),
+          button_id: parseInt(button_id ?? '0'),
+          total_attempts: 5,
+          first_attempt: currentTime,
+          last_attempt: currentTime,
+        },
+      });
+    }
 
     throw new TRPCError({
       code: "TOO_MANY_REQUESTS",
