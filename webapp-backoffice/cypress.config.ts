d2356873548ae29dd17b67e667886b38282514a5
YoannNumericite <y.lebars@numericite.eu>
2024-09-06 11:38:51 +0200

    fix: implement docker compose for cypress testing

--- webapp-backoffice/cypress.config.ts
+++ webapp-backoffice/cypress.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
+		reporter: 'spec',
+		reporterOptions: {
+		  toConsole: true
+		},
+		screenshotOnRunFailure: false,
+		viewportWidth: 1280,
+		viewportHeight: 720,
 		setupNodeEvents(on, config) {},
 		baseUrl: process.env.NEXTAUTH_URL
 	}
