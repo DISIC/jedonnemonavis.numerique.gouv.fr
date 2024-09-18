0c2da0ad032fd794b31f213da9121ec0981a1787
FredericNumericite <frederic@numericite.eu>
2024-09-18 16:28:51 +0200

    fix: display issue for table headers in safari

--- webapp-form/src/components/form/elements/ArrayRadio.tsx
+++ webapp-form/src/components/form/elements/ArrayRadio.tsx
@@ -356,9 +356,6 @@ const useStyles = tss
       "th:last-child, td:last-child": {
         width: "10rem",
       },
-      "th:not(:first-of-type):not(:last-child)": {
-        width: "calc((100% - 20rem) / 6)",
-      },
     },
     reviewInputLabel: {
       border: `1px solid ${fr.colors.decisions.background.alt.grey.hover}`,
