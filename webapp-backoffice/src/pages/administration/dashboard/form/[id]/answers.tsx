c0790d000bf62b1982b2b52d1febe32a65cb3508
YoannNumericite <y.lebars@numericite.eu>
2024-10-02 19:36:12 +0200

    feat: implement custom form reviews

--- webapp-backoffice/src/pages/administration/dashboard/form/[id]/answers.tsx
+++ webapp-backoffice/src/pages/administration/dashboard/form/[id]/answers.tsx
@@ -8,6 +8,8 @@ import { Form } from '@/prisma/generated/zod';
 import FormLayout from '@/src/layouts/Form/FormLayout';
 import { trpc } from '@/src/utils/trpc';
 import { Loader } from '@/src/components/ui/Loader';
+import { TypeBlocksDescription } from '@/src/utils/content';
+import { formatDateToFrenchString } from '@/src/utils/tools';
 
 interface Props {
 	form: Form;
@@ -20,7 +22,7 @@ const FormBuilder = (props: Props) => {
 	const [page, setPage] = React.useState<number>(0);
 	const [numberPerPage, setNumberPerPage] = React.useState<number>(10);
 
-	const { classes } = useStyles();
+	const { classes, cx } = useStyles();
 
 	const {
 		data: reviewsResult,
@@ -76,6 +78,20 @@ const FormBuilder = (props: Props) => {
 		metadata: { blockCount: blockCount }
 	} = blocksResult;
 
+	React.useEffect(() => {
+		console.log('formBlocks : ', formBlocks);
+	}, [formBlocks]);
+
+	const questionTypes = TypeBlocksDescription.filter(
+		t => t.category === 'Questions'
+	).map(t => t.type);
+	const filteredBlocsLabel = formBlocks
+		.filter(b => questionTypes.includes(b.type_bloc))
+		.map(b => b.content);
+	const filteredBlocsID = formBlocks
+		.filter(b => questionTypes.includes(b.type_bloc))
+		.map(b => b.id);
+
 	return (
 		<FormLayout form={form}>
 			<Head>
@@ -90,10 +106,36 @@ const FormBuilder = (props: Props) => {
 					<h1>Form Réponses</h1>
 				</div>
 			</div>
-			<div>
-				<div className={fr.cx('fr-py-10v')}>
-					<Loader />
+			{isLoadingBlocks && isLoadingReviews && (
+				<div>
+					<div className={fr.cx('fr-py-10v')}>
+						<Loader />
+					</div>
 				</div>
+			)}
+			<div className={cx(classes.scrollContainer)}>
+				<div className={cx(fr.cx('fr-text--bold'), classes.scrollContent)}>
+					{['Date', 'Heure'].concat(filteredBlocsLabel as string[]).map(b => (
+						<div className={cx(fr.cx('fr-p-5v'), classes.item)}>{b}</div>
+					))}
+				</div>
+				{reviews.map(r => (
+					<div className={cx(classes.scrollContent)}>
+						<div className={cx(fr.cx('fr-p-5v'), classes.item)}>
+							{formatDateToFrenchString(
+								r.created_at?.toISOString().split('T')[0] || ''
+							)}
+						</div>
+						<div className={cx(fr.cx('fr-p-5v'), classes.item)}>
+							{r.created_at?.toLocaleTimeString('fr-FR')}
+						</div>
+						{filteredBlocsID.map(f => (
+							<div className={cx(fr.cx('fr-p-5v'), classes.item)}>
+								{r.answers.find(a => a.block_id === f)?.content}
+							</div>
+						))}
+					</div>
+				))}
 			</div>
 		</FormLayout>
 	);
@@ -109,6 +151,31 @@ const useStyles = tss.withName(FormBuilder.name).create({
 		display: 'flex',
 		flexDirection: 'column',
 		gap: fr.spacing('10v')
+	},
+	scrollContainer: {
+		overflowX: 'auto',
+		width: '100%',
+		padding: '10px'
+	},
+	scrollContent: {
+		display: 'flex',
+		flexWrap: 'nowrap',
+		gap: '10px',
+		padding: '10px',
+		width: 'max-content'
+	},
+	item: {
+		width: '200px',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: '5px 0',
+		'&:nth-of-type(1), &:nth-of-type(2)': {
+			width: '100px'
+		},
+		'&:nth-of-type(n+3)': {
+			width: '250px'
+		}
 	}
 });
 
