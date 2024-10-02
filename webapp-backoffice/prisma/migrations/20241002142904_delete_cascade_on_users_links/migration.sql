99f2f39dd14dc608d95aada5d3ce0e6a7d116e2f
YoannNumericite <y.lebars@numericite.eu>
2024-10-02 19:53:31 +0200

    fix: fix migraion

--- webapp-backoffice/prisma/migrations/20241002142904_delete_cascade_on_users_links/migration.sql
+++ webapp-backoffice/prisma/migrations/20241002142904_delete_cascade_on_users_links/migration.sql
@@ -10,20 +10,6 @@ ALTER TABLE "UserResetToken" DROP CONSTRAINT "UserResetToken_user_id_fkey";
 -- DropForeignKey
 ALTER TABLE "UserValidationToken" DROP CONSTRAINT "UserValidationToken_user_id_fkey";
 
--- CreateTable
-CREATE TABLE "ReviewCustom" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "ReviewCustom_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "AnswerCustom" (
-    "id" SERIAL NOT NULL,
-
-    CONSTRAINT "AnswerCustom_pkey" PRIMARY KEY ("id")
-);
-
 -- AddForeignKey
 ALTER TABLE "UserRequest" ADD CONSTRAINT "UserRequest_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
