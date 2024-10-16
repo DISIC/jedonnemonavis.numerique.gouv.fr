222e86077b8c3791cfa978f25c767d8cf8917874
BrianRid <brianridolce@gmail.com>
2024-10-16 16:22:56 +0200

    feat: adding log logic to review line onclick on more infos

---
+++ webapp-backoffice/src/server/routers/reviewViewLog.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { router, protectedProcedure } from '@/src/server/trpc';
+
+export const reviewViewLogRouter = router({
+	create: protectedProcedure
+		.input(
+			z.object({
+				review_id: z.number(),
+				review_created_at: z.date()
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			const { review_id, review_created_at } = input;
+
+			const reviewViewLog = await ctx.prisma.reviewViewLog.create({
+				data: {
+					user_id: parseInt(ctx.session.user.id),
+					review_id,
+					review_created_at
+				}
+			});
+
+			return reviewViewLog;
+		})
+});
