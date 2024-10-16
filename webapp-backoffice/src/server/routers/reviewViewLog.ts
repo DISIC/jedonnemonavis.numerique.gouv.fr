39ea2f4ef1e5cb82d9dc77b7588d6c6ea41553c8
BrianRid <brianridolce@gmail.com>
2024-10-16 16:45:16 +0200

    feat: adding check on create to avoid multicreation

--- webapp-backoffice/src/server/routers/reviewViewLog.ts
+++ webapp-backoffice/src/server/routers/reviewViewLog.ts
@@ -12,13 +12,33 @@ export const reviewViewLogRouter = router({
 		.mutation(async ({ ctx, input }) => {
 			const { review_id, review_created_at } = input;
 
-			const reviewViewLog = await ctx.prisma.reviewViewLog.create({
-				data: {
-					user_id: parseInt(ctx.session.user.id),
+			const user = await ctx.prisma.user.findUnique({
+				where: {
+					id: parseInt(ctx.session.user.id)
+				}
+			});
+
+			const existingReviewViewLog = await ctx.prisma.reviewViewLog.findFirst({
+				where: {
+					user_id: user?.id,
 					review_id,
 					review_created_at
 				}
 			});
+
+			let reviewViewLog;
+
+			if (existingReviewViewLog) {
+				return;
+			} else {
+				reviewViewLog = await ctx.prisma.reviewViewLog.create({
+					data: {
+						user_id: parseInt(ctx.session.user.id),
+						review_id,
+						review_created_at
+					}
+				});
+			}
 
 			return reviewViewLog;
 		})
