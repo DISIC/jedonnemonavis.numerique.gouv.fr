6b5fcf0c21a92d3697de6974834f933d3681223d
BrianRid <brianridolce@gmail.com>
2025-01-09 14:20:25 +0100

    fix: imports in launcher

--- webapp-backoffice/cypress/e2e/jdma/launcher.cy.js
+++ webapp-backoffice/cypress/e2e/jdma/launcher.cy.js
@@ -1,7 +1,7 @@
-// import './bo/home.cy.js';
-// import './bo/admin.cy.js';
-// import './bo/register.cy.js';
-// import './form/review.cy.js';
+import './bo/home.cy.js';
+import './bo/admin.cy.js';
+import './bo/register.cy.js';
+import './form/review.cy.js';
 import './bo/reviewCheck.cy.js';
-// import './bo/account.cy.js';
-// import './bo/users.cy.js';
+import './bo/account.cy.js';
+import './bo/users.cy.js';
