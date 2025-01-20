5943c829c2f114902d2c9a1956bcaa595f1788f4
BrianRid <brianridolce@gmail.com>
2025-01-20 16:48:25 +0100

    fix: converting to text status

--- webapp-backoffice/prisma/migrations/20250120101043_revamp_accessright_status/migration.sql
+++ webapp-backoffice/prisma/migrations/20250120101043_revamp_accessright_status/migration.sql
@@ -8,14 +8,15 @@
 -- AlterEnum
 BEGIN;
 CREATE TYPE "RightAccessStatus_new" AS ENUM ('carrier_admin', 'carrier_user', 'removed');
-ALTER TABLE "AccessRight" ALTER COLUMN "status" TYPE "RightAccessStatus_new" USING
+ALTER TABLE "AccessRight" ALTER COLUMN "status" TYPE "RightAccessStatus_new" USING (
   CASE
-    WHEN status = 'admin' THEN 'carrier_admin'
-    WHEN status = 'user' THEN 'carrier_user'
-    WHEN status = 'carrier' THEN 'carrier_user'
-    WHEN status = 'removed' THEN 'removed'
-    ELSE 'carrier_user'
-  END::text::"RightAccessStatus_new";
+    WHEN status::text = 'admin' THEN 'carrier_admin'::text
+    WHEN status::text = 'user' THEN 'carrier_user'::text
+    WHEN status::text = 'carrier' THEN 'carrier_user'::text
+    WHEN status::text = 'removed' THEN 'removed'::text
+    ELSE 'carrier_user'::text
+  END
+)::text::"RightAccessStatus_new";
 ALTER TYPE "RightAccessStatus" RENAME TO "RightAccessStatus_old";
 ALTER TYPE "RightAccessStatus_new" RENAME TO "RightAccessStatus";
 DROP TYPE "RightAccessStatus_old";
@@ -26,8 +27,18 @@ COMMIT;
 BEGIN;
 CREATE TYPE "UserRole_new" AS ENUM ('superadmin', 'admin', 'user');
 ALTER TABLE "User" ALTER COLUMN "role" DROP DEFAULT;
-ALTER TABLE "User" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
-ALTER TABLE "ApiKey" ALTER COLUMN "scope" TYPE "UserRole_new" USING ("scope"::text::"UserRole_new");
+ALTER TABLE "User" ALTER COLUMN "role" TYPE "UserRole_new" USING (
+  CASE
+    WHEN role::text = 'viewer' THEN 'user'::text
+    ELSE role::text
+  END
+)::text::"UserRole_new";
+ALTER TABLE "ApiKey" ALTER COLUMN "scope" TYPE "UserRole_new" USING (
+  CASE
+    WHEN scope::text = 'viewer' THEN 'user'::text
+    ELSE scope::text
+  END
+)::text::"UserRole_new";
 ALTER TYPE "UserRole" RENAME TO "UserRole_old";
 ALTER TYPE "UserRole_new" RENAME TO "UserRole";
 DROP TYPE "UserRole_old";
