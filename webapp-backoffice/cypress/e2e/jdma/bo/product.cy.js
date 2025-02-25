65a6f0f66559d4ab54a5d83f81a9641ef80716b2
BrianRid <brianridolce@gmail.com>
2025-02-25 11:05:16 +0100

    fix: clean code

--- webapp-backoffice/cypress/e2e/jdma/bo/product.cy.js
+++ webapp-backoffice/cypress/e2e/jdma/bo/product.cy.js
@@ -22,22 +22,14 @@ describe('Product', () => {
 	it('should display the jdma button code', () => {
 		cy.visit(app_url + '/administration/dashboard/product/5/buttons');
 
-		cy.wait(5000);
-
 		cy.get('h1').should('contain', 'e2e-jdma-service-test-1').should('exist');
 
-		console.log('h1 is correct');
-
 		cy.get('.fr-card').find('.fr-btn--secondary').click({ force: true });
-
-		console.log('button is clicked');
 
 		cy.get('.MuiMenu-paper')
 			.find('.MuiMenuItem-root')
 			.contains('Voir le code')
 			.click({ force: true });
-
-		console.log('code is clicked');
 
 		cy.get('.fr-grid-row h5')
 			.contains('Thème clair')
@@ -45,21 +37,16 @@ describe('Product', () => {
 			.find('img')
 			.should('have.attr', 'src', '/assets/bouton-bleu-clair.svg');
 
-		console.log('theme is correct');
 		cy.contains('label.fr-label', 'Blanc')
 			.parent('.fr-radio-group')
 			.find('input[type="radio"]')
 			.check({ force: true });
-
-		console.log('radio is checked');
 
 		cy.get('.fr-grid-row h5')
 			.contains('Thème clair')
 			.parent()
 			.find('img')
 			.should('have.attr', 'src', '/assets/bouton-blanc-clair.svg');
-
-		console.log('theme is correct');
 	});
 });
 
