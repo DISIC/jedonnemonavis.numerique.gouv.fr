c30e7a6c39eeab1e412d4f84ec14c0423d9f638c
YoannNumericite <y.lebars@numericite.eu>
2025-02-26 17:30:16 +0100

    feat: monter version DSFR

---
+++ webapp-backoffice/prisma/migrations/20250226161607_test/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "ReviewViewLog" DROP CONSTRAINT "ReviewViewLog_review_id_review_created_at_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ReviewViewLog" DROP CONSTRAINT "ReviewViewLog_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "ReviewViewLog" ADD CONSTRAINT "ReviewViewLog_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReviewViewLog" ADD CONSTRAINT "ReviewViewLog_review_id_review_created_at_fkey" FOREIGN KEY ("review_id", "review_created_at") REFERENCES "Review"("id", "created_at") ON DELETE CASCADE ON UPDATE CASCADE;
