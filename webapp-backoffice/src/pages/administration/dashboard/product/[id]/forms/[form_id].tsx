5fbe920d88a04d67d3f7ea68652112b36b8c23dc
Clément <clement@numericite.eu>
2025-03-20 10:34:21 +0100

    feat: header of form page

--- webapp-backoffice/src/pages/administration/dashboard/product/[id]/forms/[form_id].tsx
+++ webapp-backoffice/src/pages/administration/dashboard/product/[id]/forms/[form_id].tsx
@@ -57,7 +57,7 @@ const ProductFormPage = (props: Props) => {
 			<Breadcrumb
 				currentPageLabel={form.form_template.title}
 				segments={breadcrumbSegments}
-				className={fr.cx('fr-mb-6v')}
+				className={fr.cx('fr-mb-4v')}
 			/>
 			<Link
 				href={breadcrumbSegments[1].linkProps.href}
@@ -72,6 +72,34 @@ const ProductFormPage = (props: Props) => {
 					Annuler et retourner au service
 				</Button>
 			</Link>
+			<div className={fr.cx('fr-grid-row', 'fr-grid-row--gutters', 'fr-my-6v')}>
+				<div className={fr.cx('fr-col-8')}>
+					<h1 className={fr.cx('fr-mb-0')}>{form.form_template.title}</h1>
+				</div>
+				<div className={cx(classes.headerButtons, fr.cx('fr-col-4'))}>
+					<Button priority="secondary">
+						<Link
+							href={`${process.env.NEXT_PUBLIC_FORM_APP_URL}/Demarches/${form.product_id}?iframe=true`}
+							target="_blank"
+						>
+							Prévisuliser
+						</Link>
+					</Button>
+					<Button
+						priority="primary"
+						iconId="fr-icon-computer-line"
+						iconPosition="right"
+					>
+						Publier
+					</Button>
+				</div>
+				<div className={fr.cx('fr-col-12')}>
+					<p>
+						Ici, un texte décrivant brièvement le modèle Évaluation et usager,
+						le type de données récoltées et comment les exploiter.
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
@@ -79,6 +107,18 @@ const ProductFormPage = (props: Props) => {
 const useStyles = tss.withName(ProductFormPage.name).create({
 	backLink: {
 		backgroundImage: 'none'
+	},
+	headerButtons: {
+		display: 'flex',
+		justifyContent: 'end',
+		gap: fr.spacing('4v'),
+		alignSelf: 'center',
+		button: {
+			a: {
+				display: 'flex',
+				alignItems: 'center'
+			}
+		}
 	}
 });
 
