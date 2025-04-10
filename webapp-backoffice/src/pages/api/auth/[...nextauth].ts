7a28d102ac75e33efeed5def57cb0dc14650a091
YoannNumericite <y.lebars@numericite.eu>
2025-04-10 11:56:30 +0200

    feat: remove scope phone proconnect

--- webapp-backoffice/src/pages/api/auth/[...nextauth].ts
+++ webapp-backoffice/src/pages/api/auth/[...nextauth].ts
@@ -202,7 +202,7 @@ export const authOptions: NextAuthOptions = {
 			authorization: {
 				url: `https://${process.env.PROCONNECT_DOMAIN}/api/v2/authorize`,
 				params: {
-					scope: 'openid email given_name usual_name phone siret'
+					scope: 'openid email given_name usual_name siret'
 				}
 			},
 			token: `https://${process.env.PROCONNECT_DOMAIN}/api/v2/token`,
