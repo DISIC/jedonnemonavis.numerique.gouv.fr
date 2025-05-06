4bc53996cd7ac74325454c5641aa7675f8a4e18a
dan1M <maneadaniel555@gmail.com>
2025-05-06 16:59:46 +0200

    feat: indicators architecture redesigned, icons added for each indicator

--- webapp-backoffice/src/components/dashboard/Stats/ObservatoireStats.tsx
+++ webapp-backoffice/src/components/dashboard/Stats/ObservatoireStats.tsx
@@ -1,6 +1,7 @@
 import { getPercentageFromValue, getReadableValue } from '@/src/utils/tools';
 import { trpc } from '@/src/utils/trpc';
-import { fr } from '@codegouvfr/react-dsfr';
+import { fr, FrIconClassName, RiIconClassName } from '@codegouvfr/react-dsfr';
+import Badge from '@codegouvfr/react-dsfr/Badge';
 import { Skeleton, Tooltip } from '@mui/material';
 import { tss } from 'tss-react';
 
@@ -16,6 +17,7 @@ type StatField = {
 	slug: 'satisfaction' | 'comprehension' | 'contactReachability' | 'contactSatisfaction' | 'autonomy';
 	value: number;
 	tooltip: string;
+	icon: RiIconClassName | FrIconClassName;
 };
 
 const ObservatoireStats = ({
@@ -74,16 +76,16 @@ const ObservatoireStats = ({
 		return 'Bien';
 	};
 
-	const getClassFromValue = (value: number, slug: StatField['slug']) => {
+	const getSeverityFromValue = (value: number, slug: StatField['slug']) => {
 		if (slug === 'contactReachability') {
-			if (value < 7) return classes.pasBien;
-			if (value < 8.5) return classes.moyen;
-			return classes.bien;
+			if (value < 7) return 'error';
+			if (value < 8.5) return 'new';
+			return 'success';
 		}
 
-		if (value < 5) return classes.pasBien;
-		if (value < 8) return classes.moyen;
-		return classes.bien;
+		if (value < 5) return 'error';
+		if (value < 8) return 'new';
+		return 'success';
 	};
 
 	const statFields: StatField[] = [
@@ -92,68 +94,67 @@ const ObservatoireStats = ({
 			slug: 'satisfaction',
 			value: resultStatsObservatoire.data.satisfaction,
 			tooltip:
-				'Pour calculer la note de satisfaction, nous réalisons une moyenne des réponses données à la question « De façon générale, comment ça s’est passé ? » en attribuant une note sur 10 à chaque option de réponses proposée dans le questionnaire.'
+				'Pour calculer la note de satisfaction, nous réalisons une moyenne des réponses données à la question « De façon générale, comment ça s’est passé ? » en attribuant une note sur 10 à chaque option de réponses proposée dans le questionnaire.',
+			icon: 'ri-emoji-sticker-line'
 		},
 		{
 			label: 'Simplicité du langage',
 			slug: 'comprehension',
 			value: resultStatsObservatoire.data.comprehension,
 			tooltip:
-				"Pour calculer la note de simplicité du langage, nous réalisons une moyenne des réponses données à la question « Qu'avez-vous pensé des informations et des instructions fournies ? » en attribuant une note sur 10 aux cinq réponses proposées dans le questionnaire."
+				"Pour calculer la note de simplicité du langage, nous réalisons une moyenne des réponses données à la question « Qu'avez-vous pensé des informations et des instructions fournies ? » en attribuant une note sur 10 aux cinq réponses proposées dans le questionnaire.",
+			icon: 'ri-speak-line'
 		},
 		{
 			label: 'Aide joignable',
 			slug: 'contactReachability',
 			value: resultStatsObservatoire.data.contact_reachability,
 			tooltip:
-				'Cette évaluation correspond à la part d’usagers en pourcentage ayant réussi à joindre l’administration pour l’aider dans la réalisation de sa démarche. La part est calculée grâce aux réponses obtenues à la question  « Quand vous avez cherché de l’aide, avez-vous réussi à joindre l’administration ? ». '
+				'Cette évaluation correspond à la part d’usagers en pourcentage ayant réussi à joindre l’administration pour l’aider dans la réalisation de sa démarche. La part est calculée grâce aux réponses obtenues à la question  « Quand vous avez cherché de l’aide, avez-vous réussi à joindre l’administration ? ».',
+			icon: 'ri-customer-service-line'
 		},
 		{
 			label: 'Aide efficace',
 			slug: 'contactSatisfaction',
 			value: resultStatsObservatoire.data.contact_satisfaction,
 			tooltip:
-				'Cette évaluation correspond à la qualité de l’aide obtenue de la part de l’administration. Nous réalisons une moyenne des réponses données à la question « Comment évaluez-vous la qualité de l’aide que vous avez obtenue de la part de l’administration ? » en attribuant une note sur 10 à chaque option de réponses proposée dans le questionnaire.'
+				'Cette évaluation correspond à la qualité de l’aide obtenue de la part de l’administration. Nous réalisons une moyenne des réponses données à la question « Comment évaluez-vous la qualité de l’aide que vous avez obtenue de la part de l’administration ? » en attribuant une note sur 10 à chaque option de réponses proposée dans le questionnaire.',
+			icon: 'ri-customer-service-line'
 		},
 		{
 			label: "Niveau d'autonomie",
 			slug: 'autonomy',
 			value: resultStatsObservatoire.data.autonomy,
 			tooltip:
-				'Comme la note de satisfaction usager, cette note est calculée sur la base des retours usagers récoltés via le questionnaire de satisfaction (bouton «je donne mon avis», qui se trouve à la fin de la démarche).'
+				'Comme la note de satisfaction usager, cette note est calculée sur la base des retours usagers récoltés via le questionnaire de satisfaction (bouton «je donne mon avis», qui se trouve à la fin de la démarche).',
+			icon: 'ri-chat-smile-line'
 		}
 	];
 
 	const getStatsDisplay = (field: StatField) => {
 		if (isLoadingStatsObservatoire || isRefetchingStatsObservatoire) {
 			return (
-				<Skeleton className={classes.skeleton} height={145} width={'80%'} />
+				<div className={classes.statsDisplay}>
+					<Skeleton className={classes.skeleton} height={64} width={'100%'} />
+				</div>
 			);
 		}
 		if (!!resultStatsObservatoire.metadata[`${field.slug}_count`]) {
 			return (
-				<div>
-					<p
-						className={cx(
-							classes.value,
-							field.slug !== 'autonomy'
-								? getClassFromValue(field.value * (field.slug === 'contactReachability' ? 10 : 1), field.slug)
-								: cx(fr.cx('fr-mb-18v'))
-						)}
-					>
+				<div className={classes.statsDisplay}>
+					<p className={cx(classes.value)}>
 						{['autonomy', 'contactReachability'].includes(field.slug)
-							? `${getPercentageFromValue(field.value * (field.slug === 'contactReachability' ? 10 : 1))}%`
+							? `${getPercentageFromValue(field.value * (field.slug === 'contactReachability' ? 10 : 1))} %`
 							: `${getReadableValue(field.value)} / 10`}
 					</p>
 					{field.slug !== 'autonomy' && (
-						<p
-							className={cx(
-								classes.intention,
-								getClassFromValue(field.value * (field.slug === 'contactReachability' ? 10 : 1), field.slug)
-							)}
+						<Badge 
+							severity={getSeverityFromValue(field.value * (field.slug === 'contactReachability' ? 10 : 1), field.slug)} 
+							noIcon 
+							className={classes.intention}
 						>
 							{getLabelFromValue(field.value * (field.slug === 'contactReachability' ? 10 : 1), field.slug)}
-						</p>
+						</Badge>
 					)}
 				</div>
 			);
@@ -182,37 +183,25 @@ const ObservatoireStats = ({
 			<h3 className={classes.title}>
 				Les indicateurs de vos démarches essentielles
 			</h3>
-			<div
-				className={fr.cx(
-					'fr-grid-row',
-					'fr-grid-row--gutters',
-				)}
-			>
+			<div className={classes.contentContainer}>
 				{statFields.map((field, index) => (
-					<div
-						key={index}
-						className={fr.cx(
-							'fr-col',
-							'fr-col-sm-12',
-							'fr-col-md-6',
-							'fr-col-lg-3'
-						)}
-					>
-						<div className={cx(classes.content)}>
-							<label className={cx(classes.label)}>
-								<h5>{field.label}</h5>
-								<Tooltip placement="top" title={field.tooltip} tabIndex={0}>
-									<span
-										className={fr.cx(
-											'fr-icon-information-line',
-											'fr-icon--md',
-											'fr-ml-1v'
-										)}
-									/>
-								</Tooltip>
-							</label>
-							{getStatsDisplay(field)}
+					<div key={index} className={cx(classes.content)}>
+						<div className={cx(classes.indicatorIcon, cx(fr.cx('fr-mr-6v')))}>
+							<i className={cx(fr.cx(field.icon), classes.icon)} />
 						</div>
+						<label className={cx(classes.label)}>
+							<h5>{field.label}</h5>
+							<Tooltip placement="top" title={field.tooltip} tabIndex={0}>
+								<span
+									className={fr.cx(
+										'fr-icon-information-line',
+										'fr-icon--md',
+										'fr-ml-1v'
+									)}
+								/>
+							</Tooltip>
+						</label>
+						{getStatsDisplay(field)}
 					</div>
 				))}
 			</div>
@@ -222,42 +211,68 @@ const ObservatoireStats = ({
 
 const useStyles = tss.create({
 	card: {
-		marginTop: fr.spacing('15v'),
+		marginTop: fr.spacing('16v'),
 		paddingBottom: fr.spacing('20v'),
 		borderBottom: `1px solid ${fr.colors.decisions.border.default.grey.default}`
 	},
 	title: {
-		marginBottom: fr.spacing('15v')
+		marginBottom: fr.spacing('4v')
+	},
+	contentContainer:{
+		display: 'flex',
+		flexDirection: 'column',
+		gap: fr.spacing('4v'),
 	},
 	content: {
 		textAlign: 'center',
-		padding: '0 1.7rem',
-		borderRadius: '0.5rem',
+		padding: fr.spacing('3v'),
 		display: 'flex',
-		flexDirection: 'column',
+		alignItems: 'center',
 		height: '100%',
 		justifyContent: 'space-between',
+		border: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
 	},
 	label: {
+		flex: 2,
 		display: 'flex',
 		alignItems: 'center',
 		fontWeight: 'bold',
-		marginBottom: '0.5rem',
 		minHeight: '3rem',
 		h5: {
 			margin: 0
 		}
 	},
+	statsDisplay: {
+		flex: 1,
+		display: 'flex',
+		height: '100%',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		gap: fr.spacing('8v'),
+	},
 	intention: {
-		...fr.typography[18].style,
-		fontWeight: 'bold',
-		marginTop: '1rem',
-		textTransform: 'uppercase'
+		minWidth: '7rem',
+		justifyContent: 'center',
 	},
 	value: {
 		fontSize: '2rem',
 		fontWeight: 'bold',
-		marginTop: '2rem'
+		margin: 0,
+	},
+	indicatorIcon: {
+		width: '4rem',
+		height: '4rem',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: '50%',
+		backgroundColor: fr.colors.decisions.artwork.decorative.blueFrance.default,
+	},
+	icon: {
+		color: fr.colors.decisions.background.flat.blueFrance.default,
+		'::before': {
+			'--icon-size': '2rem',
+		}
 	},
 	skeleton: {
 		margin: 'auto'
