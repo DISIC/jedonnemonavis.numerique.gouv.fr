b7da004228b8f1e3e8768338ab4461457144c0d3
dan1M <maneadaniel555@gmail.com>
2025-05-14 17:09:25 +0200

    fix: large legends made wrap, stacked vertical bar chart fix mobile

--- webapp-backoffice/src/components/chart/StackedVerticalBarChart.tsx
+++ webapp-backoffice/src/components/chart/StackedVerticalBarChart.tsx
@@ -33,8 +33,10 @@ const renderLegend = (props: any, sortOrder: { [key: string]: number }) => {
 		<div
 			style={{
 				display: 'flex',
-				gap: '25px',
-				marginLeft: '25px'
+				gap: '20px',
+				marginLeft: '25px',
+				marginBottom: fr.spacing("6v"),
+				flexWrap: 'wrap',
 			}}
 		>
 			{payload
@@ -101,14 +103,16 @@ const StackedVerticalBarChart = ({
 	};
 	total: number;
 }) => {
+	const isMobile = window.innerWidth <= fr.breakpoints.getPxValues().md;
+
 	return (
-		<ResponsiveContainer width="100%" height={300}>
+		<ResponsiveContainer width="100%" height={isMobile ? 400 : 350}>
 			<BarChart
 				role="img"
 				title="Graphique: Répartition des réponses"
 				data={data}
 				layout="vertical"
-				margin={{ left: -25, bottom: 5 }}
+				margin={{ left: -25, bottom: 5, top: 20 }}
 			>
 				<CartesianGrid horizontal={false} strokeDasharray="3 3" />
 				<XAxis
@@ -146,7 +150,7 @@ const StackedVerticalBarChart = ({
 				<Legend
 					verticalAlign="top"
 					align="left"
-					height={60}
+					// height={60}
 					content={payload => renderLegend(payload, sortOrder)}
 				/>
 				{dataKeys
