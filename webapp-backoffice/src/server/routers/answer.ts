a6b151f28448f6a18c6192f8a6aea323d04b252a
Clément <clement@numericite.eu>
2025-06-12 17:56:03 +0200

    fix: form_id old version to 2

--- webapp-backoffice/src/server/routers/answer.ts
+++ webapp-backoffice/src/server/routers/answer.ts
@@ -543,7 +543,7 @@ export const answerRouter = router({
 				query: queryCountByFieldCode({
 					...input,
 					...(form?.legacy
-						? { OR: [{ form_id: form_id }, { form_id: null }] }
+						? { OR: [{ form_id: form_id }, { form_id: 2 }] }
 						: { form_id })
 				})
 			});
