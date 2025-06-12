05326654685662fde32ae0d78b84f59b4f1383f0
Clément <clement@numericite.eu>
2025-06-12 14:16:21 +0200

    fix: remove tools unused imports

--- webapp-backoffice/src/utils/tools.ts
+++ webapp-backoffice/src/utils/tools.ts
@@ -1,11 +1,9 @@
-import { matchSorter } from 'match-sorter';
-import { trpc } from './trpc';
 import { AnswerIntention, TypeAction } from '@prisma/client';
 import { JsonValue } from '@prisma/client/runtime/library';
-import { off } from 'process';
-import { FormConfigWithRelations } from '@/prisma/generated/zod';
-import { FormConfigWithChildren } from '../types/prismaTypesExtended';
+import { matchSorter } from 'match-sorter';
 import { FormConfigHelper } from '../pages/administration/dashboard/product/[id]/forms/[form_id]/edit';
+import { FormConfigWithChildren } from '../types/prismaTypesExtended';
+import { trpc } from './trpc';
 
 export function isValidDate(dateString: string) {
 	var regex = /^\d{4}-\d{2}-\d{2}$/;
