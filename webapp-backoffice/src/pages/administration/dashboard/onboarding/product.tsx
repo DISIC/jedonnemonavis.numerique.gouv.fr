9f5b926e9caa9369794007b158e82ea6cd79bb86
dan1M <maneadaniel555@gmail.com>
2025-10-20 18:01:12 +0200

    feat: base onboarding layout (wip) used for pages from onboarding

---
+++ webapp-backoffice/src/pages/administration/dashboard/onboarding/product.tsx
@@ -0,0 +1,12 @@
+import OnboardingLayout from '@/src/layouts/Onboarding/OnboardingLayout';
+import React from 'react';
+
+const NewProduct = () => {
+	return (
+		<OnboardingLayout>
+			<div>New Product</div>
+		</OnboardingLayout>
+	);
+};
+
+export default NewProduct;
