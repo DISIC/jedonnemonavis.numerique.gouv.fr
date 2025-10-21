c909794ac664bbc1d43f02893863323ea69867dd
dan1M <maneadaniel555@gmail.com>
2025-10-21 12:08:34 +0200

    feat: layout update with some actions

--- webapp-backoffice/src/pages/administration/dashboard/onboarding/product.tsx
+++ webapp-backoffice/src/pages/administration/dashboard/onboarding/product.tsx
@@ -1,9 +1,15 @@
 import OnboardingLayout from '@/src/layouts/Onboarding/OnboardingLayout';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const NewProduct = () => {
+	const router = useRouter();
 	return (
-		<OnboardingLayout>
+		<OnboardingLayout
+			showActions
+			isCancelable
+			onCancel={() => router.push('/administration/dashboard/products')}
+		>
 			<div>New Product</div>
 		</OnboardingLayout>
 	);
