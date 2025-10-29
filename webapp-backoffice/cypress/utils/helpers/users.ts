4b855f5414120bb8405ac583d905c141b1c2558a
dan1M <maneadaniel555@gmail.com>
2025-10-29 14:07:25 +0100

    fix: tests

--- webapp-backoffice/cypress/utils/helpers/users.ts
+++ webapp-backoffice/cypress/utils/helpers/users.ts
@@ -1,7 +1,11 @@
 import { selectors } from '../selectors';
+import { appUrl } from '../variables';
+import { tryCloseNewsModal } from './common';
 
 export function navigateToCreatedProduct() {
-	cy.url().should('include', selectors.dashboard.products);
+	cy.visit(`${appUrl}${selectors.url.products}`);
+	tryCloseNewsModal();
+	cy.url().should('include', selectors.url.products);
 	cy.get(selectors.productLink)
 		.filter(':contains("e2e-jdma-service-test-users")')
 		.should('have.length', 1)
