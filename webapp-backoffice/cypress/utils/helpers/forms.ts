bf91d1ae71973109ac82fc9e0cee48c2b1860e61
dan1M <maneadaniel555@gmail.com>
2025-11-06 16:33:06 +0100

    fix: tests

--- webapp-backoffice/cypress/utils/helpers/forms.ts
+++ webapp-backoffice/cypress/utils/helpers/forms.ts
@@ -2,7 +2,7 @@ import { selectors } from '../selectors';
 import { createProduct } from './common';
 
 export const tryCloseHelpModal = () => {
-	cy.wait(500);
+	cy.wait(1000);
 	cy.get('body').then(body => {
 		if (body.find('dialog#form-help-modal').length > 0) {
 			cy.get('dialog#form-help-modal')
