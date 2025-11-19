a741bc88df900560671c21cf0390b68403b9d5f0
dan1M <maneadaniel555@gmail.com>
2025-11-19 16:29:08 +0100

    feat: put steps into context

--- webapp-backoffice/src/contexts/OnboardingContext.tsx
+++ webapp-backoffice/src/contexts/OnboardingContext.tsx
@@ -1,17 +1,26 @@
-import React, { createContext, useContext, useState, useCallback } from 'react';
+import React, {
+	createContext,
+	useContext,
+	useState,
+	useCallback,
+	useEffect
+} from 'react';
 import { AccessRight, Form, Product } from '@prisma/client';
 import { FormWithElements } from '../types/prismaTypesExtended';
+import { onboardingStepsContent, StepContent } from '../utils/content';
 
 interface OnboardingState {
 	createdProduct?: Product;
 	createdForm?: FormWithElements;
 	createdUserAccesses?: AccessRight[];
+	steps: StepContent[];
 }
 
 interface OnboardingContextValue extends OnboardingState {
 	updateCreatedProduct: (product: Product) => void;
 	updateCreatedForm: (form: FormWithElements) => void;
 	updateCreatedUserAccesses: (accesses: AccessRight[]) => void;
+	updateSteps: (newSteps: StepContent[]) => void;
 	reset: () => void;
 }
 
@@ -31,6 +40,51 @@ export const OnboardingProvider = ({
 	const [createdForm, setCreatedForm] = useState<
 		FormWithElements | undefined
 	>();
+	const [steps, setSteps] = useState<StepContent[]>(onboardingStepsContent);
+
+	const getSlugValues = (
+		stepSlug: string
+	): { isCompleted: boolean; url: string } => {
+		switch (stepSlug) {
+			case 'product':
+				return {
+					isCompleted: Boolean(createdProduct),
+					url: '/administration/dashboard/product/new'
+				};
+			case 'access':
+				return {
+					isCompleted: Boolean(
+						createdUserAccesses && createdUserAccesses.length > 0
+					),
+					url: `/administration/dashboard/product/${createdProduct?.id}/access/new`
+				};
+			case 'form':
+				return {
+					isCompleted: Boolean(createdForm),
+					url: `/administration/dashboard/product/${createdProduct?.id}/forms/new`
+				};
+			case 'link':
+				return {
+					isCompleted: false,
+					url: `/administration/dashboard/product/${createdProduct?.id}/forms/${createdForm?.id}/new-link`
+				};
+			default:
+				return {
+					isCompleted: false,
+					url: '/administration/dashboard/product/new'
+				};
+		}
+	};
+
+	useEffect(() => {
+		setSteps(
+			steps.map(step => ({
+				...step,
+				isCompleted: getSlugValues(step.slug).isCompleted,
+				url: getSlugValues(step.slug).url
+			}))
+		);
+	}, [createdProduct, createdUserAccesses, createdForm]);
 
 	const updateCreatedProduct = useCallback((p: Product) => {
 		setCreatedProduct(p);
@@ -44,10 +98,15 @@ export const OnboardingProvider = ({
 		setCreatedUserAccesses(accesses);
 	}, []);
 
+	const updateSteps = useCallback((newSteps: StepContent[]) => {
+		setSteps(newSteps);
+	}, []);
+
 	const reset = useCallback(() => {
 		setCreatedProduct(undefined);
 		setCreatedForm(undefined);
 		setCreatedUserAccesses(undefined);
+		setSteps(onboardingStepsContent);
 	}, []);
 
 	return (
@@ -56,9 +115,11 @@ export const OnboardingProvider = ({
 				createdProduct,
 				createdForm,
 				createdUserAccesses,
+				steps,
 				updateCreatedProduct,
 				updateCreatedForm,
 				updateCreatedUserAccesses,
+				updateSteps,
 				reset
 			}}
 		>
