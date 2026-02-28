5f80261f691bf4b915a318688c9dafcab0f62825
Clément Lelong <clement@numericite.eu>
2026-02-28 15:04:26 +0100

    fix: typescript errro

--- webapp-backoffice/src/server/routers/userDetails/get-by-user.ts
+++ webapp-backoffice/src/server/routers/userDetails/get-by-user.ts
@@ -1,10 +1,6 @@
 import type { Context } from '@/src/server/trpc';
 
-export const getUserDetailsByUserQuery = async ({
-	ctx
-}: {
-	ctx: Context;
-}) => {
+export const getUserDetailsByUserQuery = async ({ ctx }: { ctx: Context }) => {
 	const userId = ctx.session!.user.id;
 	const userDetails = await ctx.prisma.userDetails.findFirst({
 		where: { userId: parseInt(userId) }
