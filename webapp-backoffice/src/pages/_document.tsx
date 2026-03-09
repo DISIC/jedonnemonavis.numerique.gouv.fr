22672e51056f4429c1491a0ce67aa2f8e9298967
dan1M <maneadaniel555@gmail.com>
2026-03-09 14:22:53 +0100

    feat : tmp implement modal script snippet to test integration

--- webapp-backoffice/src/pages/_document.tsx
+++ webapp-backoffice/src/pages/_document.tsx
@@ -12,6 +12,14 @@ const Document = (props: DocumentProps) => {
 			<body>
 				<Main />
 				<NextScript />
+				<script
+					src="https://jdma-develop.cleverapps.io/Demarches/assets/jdma-modal-widget.js"
+					data-jdma-form-url="https://jdma-develop.cleverapps.io/Demarches/avis/3092?button=9171"
+					data-jdma-button-image="https://jdma-develop.cleverapps.io/assets/buttons/button-remark-solid-light.svg"
+					data-jdma-button-label="Une remarque ?"
+					data-jdma-position="bottom-right"
+					defer
+				></script>
 			</body>
 		</Html>
 	);
