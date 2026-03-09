10e842184b592ff3f70a0b6e4404efd3f16baebc
dan1M <maneadaniel555@gmail.com>
2026-03-09 15:01:42 +0100

    feat: update jdma modal widget script to use environment variables for URLs

--- webapp-backoffice/src/pages/_document.tsx
+++ webapp-backoffice/src/pages/_document.tsx
@@ -13,9 +13,9 @@ const Document = (props: DocumentProps) => {
 				<Main />
 				<NextScript />
 				<script
-					src="https://jdma-develop.cleverapps.io/Demarches/assets/jdma-modal-widget.js"
-					data-jdma-form-url="https://jdma-develop.cleverapps.io/Demarches/avis/3092?button=9171"
-					data-jdma-button-image="https://jdma-develop.cleverapps.io/assets/buttons/button-remark-solid-light.svg"
+					src={`${process.env.NEXT_PUBLIC_FORM_APP_URL}/Demarches/assets/jdma-modal-widget.js`}
+					data-jdma-form-url={process.env.NEXT_PUBLIC_FEEDBACK_FORM_URL}
+					data-jdma-button-image={`${process.env.NEXT_PUBLIC_BO_APP_URL}/assets/buttons/button-remark-solid-light.svg`}
 					data-jdma-button-label="Une remarque ?"
 					data-jdma-position="bottom-right"
 					defer
