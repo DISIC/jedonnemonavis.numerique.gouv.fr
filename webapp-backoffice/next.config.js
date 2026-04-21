9cf48db958165f8262fbdf52836434f29a67a0ca
Antoine Lelong <azenoxe@gmail.com>
2026-04-21 14:37:25 +0200

    fix: remove instrumentation file and have a process for worker prod

--- webapp-backoffice/next.config.js
+++ webapp-backoffice/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	experimental: {
-		instrumentationHook: true
-	},
 	swcMinify: true,
 	i18n: {
 		locales: ['fr'],
--- webapp-backoffice/src/instrumentation.ts
+++
@@ -1,7 +0,0 @@
-export async function register() {
-	// In dev the worker runs as a separate process (yarn dev:worker) for hot-reload support
-	if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV !== 'development') {
-		const { startExportWorker } = await import('./workers/export-worker');
-		startExportWorker();
-	}
-}
